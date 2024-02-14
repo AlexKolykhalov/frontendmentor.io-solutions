58c0d90a94d4ebcd963cafce3b0c760e1e590662
AlexKolykhalov <ak8647@rambler.ru>
2024-02-14 15:05:03 +0300

    Included the manifest.json in static files for caching

--- advanced/multi-step-form-main/service-worker.js
+++ advanced/multi-step-form-main/service-worker.js
@@ -15,6 +15,7 @@ const APP_STATIC_RESOURCES = [
     `${path}/index.html`,
     `${path}/style.css`,
     `${path}/main.js`,
+    `${path}/manifest.json`,
     `${path}/assets/fonts/Ubuntu-Bold.ttf`,
     `${path}/assets/fonts/Ubuntu-Bold.woff`,
     `${path}/assets/fonts/Ubuntu-Bold.woff2`,
