2d651686b114b3842a3d3dd00879166ba2d1e85f
AlexKolykhalov <ak8647@rambler.ru>
2024-07-08 15:48:26 +0300

    Remove the .gitignore from root

---
+++ guru/linksharing-app/main.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+const doc = document.querySelector(".const");
+
+function main(fir, sec) {
+
+}
+
+// @ts-check
+
+const doc = document.querySelector(".const");
+
+function main(fir, sec) {
+
+}
+
+doc.addEventListener('click', (e) => {
+    return e.defaultPrevented;
+})
+ 
+doc.addEventListener('click', () => {
+    console.log('Hello');
+});
+
+function main(first, second) {
+	const one = 1;
+	const old = 0;
+}
