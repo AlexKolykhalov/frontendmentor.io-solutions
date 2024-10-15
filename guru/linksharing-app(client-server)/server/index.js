e03afb40cfe37970adf1680d26e8a56e1f6f674b
AlexKolykhalov <ak8647@rambler.ru>
2024-10-15 15:25:22 +0300

    guru:02:c-s Added some functionality
    1. Added "cookie-parser" package
    2. Added connection to DB (Sequelize)

--- guru/linksharing-app(client-server)/server/index.js
+++ guru/linksharing-app(client-server)/server/index.js
@@ -2,16 +2,24 @@
 
 import express from "express";
 import path from "path";
-
+import cookieParser from 'cookie-parser';
+import db from "./src/database/models/index.js";
 import router from "./src/routes/index.route.js";
-
+import { errorHandler } from "./src/middlewares/errorHandler.middleware.js"
 
 const __rootname = path.resolve(path.resolve(), "..");
 const port = process.env.PORT;
 const app = express();
 app.use(express.static(path.resolve(__rootname, "client")));
 app.use(express.json());
+app.use(cookieParser());
 app.use(router);
-app.use((_, res) => res.status(404).send("Not found here"));
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server has been started at port: ${port}...`));
+try {
+    await db.sequelize.authenticate();
+    app.listen(port, () => console.log(`Server (${process.env.NODE_ENV}) has been started at port: ${port}...`));
+} catch (error) {
+    console.error(error);
+}
+
