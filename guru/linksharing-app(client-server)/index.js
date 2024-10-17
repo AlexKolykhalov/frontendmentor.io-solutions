8a8cc6ef91ba9fab7369f5665edcad2db9d6646e
AlexKolykhalov <ak8647@rambler.ru>
2024-10-17 11:57:12 +0300

    guru:02:c-s Back to express structure folder

--- guru/linksharing-app(client-server)/index.js
+++ guru/linksharing-app(client-server)/index.js
@@ -7,10 +7,11 @@ import db from "./src/database/models/index.js";
 import router from "./src/routes/index.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js"
 
-const __rootname = path.resolve(path.resolve(), "..");
+// const __rootname = path.resolve(path.resolve(), "..");
 const port = process.env.PORT;
 const app = express();
-app.use(express.static(path.resolve(__rootname, "client")));
+// app.use(express.static(path.resolve(__rootname, "client")));
+app.use(express.static(path.resolve("client")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
---
+++ guru/linksharing-app(client-server)/src/routes/index.route.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import path       from "path";
+import authRouter from "./auth.route.js";
+import userRouter from "./user.route.js";
+
+// const __rootname = path.resolve(path.resolve(), "..");
+const __dirname = path.resolve();
+const router     = Router();
+
+router.use("/api",      authRouter);
+router.use("/api/user", userRouter);
+router.get("/login", (_, res) => {
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "login", "login.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "login", "login.html"));
+});
+router.get("/signup", (_, res) => {
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "signup", "signup.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "signup", "signup.html"));
+});
+router.get("/preview", (_, res) => {
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "preview", "preview.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "preview", "preview.html"));
+});
+router.get("/404", (_, res) => {
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "404", "404.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "404", "404.html"));
+});
+router.get("/:userId", (_, res) => {    
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "profile", "profile.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "profile", "profile.html"));
+});
+router.get("/", (_, res) => {
+    // res.sendFile(path.resolve(__rootname, "client", "pages", "index", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "pages", "index", "index.html"));
+});
+
+export default router;
