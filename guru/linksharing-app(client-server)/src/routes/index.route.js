d0c2b68c77fce17ca54fbcfe5357f2f00d72f6bf
AlexKolykhalov <ak8647@rambler.ru>
2024-10-21 11:59:08 +0300

    guru:02:c-s Changed structure of folder

--- guru/linksharing-app(client-server)/src/routes/index.route.js
+++ guru/linksharing-app(client-server)/src/routes/index.route.js
@@ -3,9 +3,9 @@ import path       from "path";
 import authRouter from "./auth.route.js";
 import userRouter from "./user.route.js";
 
-const __rootname = path.resolve(path.resolve(), "..");
-// const __dirname = path.resolve();
-const router     = Router();
+// const __rootname = path.resolve(path.resolve(), "..");
+const __dirname = path.resolve("guru", "linksharing-app(client-server)");
+const router    = Router();
 
 router.use("/api",      authRouter);
 router.use("/api/user", userRouter);
@@ -29,9 +29,8 @@ router.get("/:userId", (_, res) => {
     res.sendFile(path.resolve(__rootname, "public", "pages", "profile", "profile.html"));
     // res.sendFile(path.resolve(__dirname, "client", "pages", "profile", "profile.html"));
 });
-router.get("/", (_, res) => {
-    res.sendFile(path.resolve(__rootname, "public", "pages", "index", "index.html"));
-    // res.sendFile(path.resolve(__dirname, "client", "pages", "index", "index.html"));
+router.get("/", (_, res) => {    
+    res.sendFile(path.resolve(__dirname, "client", "pages", "index", "index.html"));
 });
 
 export default router;
