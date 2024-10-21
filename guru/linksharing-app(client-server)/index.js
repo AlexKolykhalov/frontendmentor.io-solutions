2ef3c948cda29ee6473952479d0d5f90106d1e15
AlexKolykhalov <ak8647@rambler.ru>
2024-10-21 15:03:59 +0300

    guru:02:c-s Changed some line of code

--- guru/linksharing-app(client-server)/index.js
+++ guru/linksharing-app(client-server)/index.js
@@ -7,10 +7,8 @@ import db from "./src/database/models/index.js";
 import router from "./src/routes/index.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js"
 
-// const __rootname = path.resolve(path.resolve(), "..");
 const port = process.env.PORT;
 const app = express();
-// app.use(express.static(path.resolve(__rootname, "client")));
 app.use(express.static(path.resolve("client")));
 app.use(express.json());
 app.use(cookieParser());
