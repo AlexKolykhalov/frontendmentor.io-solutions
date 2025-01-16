311efcfb482a0d4d809bf6480d4ddae4f9cfc6f6
AlexKolykhalov <ak8647@rambler.ru>
2025-01-16 11:46:10 +0300

    guru:03:ssr Changed folder of static files

--- guru/linksharing-app(SSR)/index.js
+++ guru/linksharing-app(SSR)/index.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import express          from "express";
-import { resolve }      from "path";
+// import { resolve }      from "path";
 import cookieParser     from "cookie-parser";
 import cors             from "cors";
 import router           from "./src/routes/index.route.js";
@@ -9,8 +9,8 @@ import { errorHandler } from "./src/middlewares/errorHandler.middleware.js"
 
 const port = process.env.PORT;
 const app  = express();
-// app.use("/public", express.static("public"));
-app.use(express.static(resolve("public")));
+app.use("/public", express.static("public"));
+// app.use(express.static(resolve("public")));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
