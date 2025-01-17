852294c74d4dc26af1d969b8ae144a70421b0312
AlexKolykhalov <ak8647@rambler.ru>
2025-01-17 10:19:38 +0300

    guru:03:ssr Removed console.log & comments

--- guru/linksharing-app(SSR)/src/routes/auth.route.js
+++ guru/linksharing-app(SSR)/src/routes/auth.route.js
@@ -11,10 +11,6 @@ const staticPath = process.env.NODE_ENV === "development" ?
       process.env.DEV_STATIC_FILES_PATH :
       process.env.PROD_STATIC_FILES_PATH;
 
-console.log(staticPath);
-console.log(`path: ${path.resolve(process.cwd(), "public")}`);
-console.log(`path of login:${path.resolve() + "/public/html/login.html"}`);
-
 const router = Router();
 
 router.post("/api/login",  emailValidator, passwordValidator, AuthController.login);
@@ -22,10 +18,7 @@ router.post("/api/signup", emailValidator, passwordValidator, AuthController.sig
 router.get("/api/refresh", refreshTokenValidator,             AuthController.refresh);
 router.get("/api/logout",                                     AuthController.logout);
 
-router.get("/login",  (_, res) => { res.sendFile("/public/html/login.html"); });
-// router.get("/login",  (_, res) => { res.sendFile("html/login.html",  { root: staticPath }); });
-// router.get("/login",  (_, res) => { res.sendFile("/var/task/guru/linksharing-app(SSR)/public/html/login.html"); });
-// router.get("/login",  (_, res) => { res.sendFile("/var/task/guru/linksharing-app(SSR)/dist/public/html/login.html"); });
+router.get("/login",  (_, res) => { res.sendFile("html/login.html",  { root: staticPath }); });
 router.get("/signup", (_, res) => { res.sendFile("html/signup.html", { root: staticPath }); });
 
 export default router;
