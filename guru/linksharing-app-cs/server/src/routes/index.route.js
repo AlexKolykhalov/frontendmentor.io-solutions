1f5a06ad4e02034ffd87d6ec9c5ef135405eb9ba
AlexKolykhalov <ak8647@rambler.ru>
2025-01-27 13:12:17 +0300

    guru:02:c-s Changed root values in routes

--- guru/linksharing-app-cs/server/src/routes/index.route.js
+++ guru/linksharing-app-cs/server/src/routes/index.route.js
@@ -4,9 +4,11 @@ import authRouter from "./auth.route.js";
 import userRouter from "./user.route.js";
 import cronRouter from "./cron.route.js";
 
-const __dirname = process.env.NODE_ENV === "development" ?
-      path.resolve(".."):
-      path.resolve("guru", "linksharing-app-cs");
+// const __dirname = process.env.NODE_ENV === "development" ?
+//       path.resolve(".."):
+//       path.resolve("guru", "linksharing-app-cs");
+
+const root = path.resolve(path.resolve(), "..") + "/client";
 const router    = Router();
 
 router.use("/api", authRouter);
@@ -14,22 +16,22 @@ router.use("/api", cronRouter);
 router.use("/api", userRouter);
 
 router.get("/login", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "login", "login.html"));
+  res.sendFile("pages/login/login.html", { root: root });
 });
 router.get("/signup", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "signup", "signup.html"));
+  res.sendFile(path.resolve(__dirname, "client", "pages", "signup", "signup.html"));
 });
 router.get("/preview", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "preview", "preview.html"));
+  res.sendFile(path.resolve(__dirname, "client", "pages", "preview", "preview.html"));
 });
 router.get("/404", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "404", "404.html"));
+  res.sendFile(path.resolve(__dirname, "client", "pages", "404", "404.html"));
 });
 router.get("/:userId", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "profile", "profile.html"));
+  res.sendFile(path.resolve(__dirname, "client", "pages", "profile", "profile.html"));
 });
-router.get("/", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "pages", "index", "index.html"));
+router.get("/", (_, res) => {  
+  res.sendFile("pages/index/index.html", { root: root });
 });
 
 export default router;
