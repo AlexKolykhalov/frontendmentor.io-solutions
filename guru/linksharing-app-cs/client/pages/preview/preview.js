0f2bdaed110b3fb9b14636909024ea04d7e6b89b
AlexKolykhalov <ak8647@rambler.ru>
2025-01-29 11:01:07 +0300

    guru:02:c-s Changed logic
    1. Renamed "getLinkInfoByName" to "getParams"
    2. Corrected some values in "createListOfPreviewLinks"

--- guru/linksharing-app-cs/client/pages/preview/preview.js
+++ guru/linksharing-app-cs/client/pages/preview/preview.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { getLinkInfoByName, showPopUpMessage } from "../../utils/utils.js";
+import { getParams, showPopUpMessage } from "../../utils/utils.js";
 
 /**
  * @typedef { import("../../../server/src/types/typedefs.js").User } User
@@ -21,53 +21,49 @@ let user;
 // ************************** 1. Events *********************************//
 
 window.addEventListener('load', async () => {
-    try {
-	const accessToken = localStorage.getItem("_t1");
-	if (!accessToken) location.replace("/login");
-	const response = await fetch(`/api/user`,
-	    {
-		method: "GET",
-		headers: {"Authorization": `Bearer ${accessToken}`},
-	    }
-	);
+  try {
+    const accessToken = localStorage.getItem("_t1");
+    if (!accessToken) location.replace("/login");
+    const response = await fetch(`/api/user`, {
+      method: "GET",
+      headers: {"Authorization": `Bearer ${accessToken}`},
+    });
+    if (response.status === 200) {
+      user = await response.json();
+      console.log(`User data: ${JSON.stringify(user, null, 2)}`);
+      if (user) populateUI(user); else showPopUpMessage("User has been deleted");
+    }
+    if (response.status === 401) {
+      const refreshResponse = await fetch(`/api/refresh`);
+      if (refreshResponse.status === 200) {
+	const token = await refreshResponse.json();
+	localStorage.setItem("_t1", token); // accessToken
+	const response = await fetch(`/api/user`, {
+	  method: "GET",
+	  headers: {"Authorization": `Bearer ${token}`},
+	});
 	if (response.status === 200) {
-	    user = await response.json();
-	    console.log(`User data: ${JSON.stringify(user, null, 2)}`);
-	    if (user) populateUI(user); else showPopUpMessage("User has been deleted");
-	}
-	if (response.status === 401) {
-	    const refreshResponse = await fetch(`/api/refresh`);
-	    if (refreshResponse.status === 200) {
-		const token = await refreshResponse.json();
-		localStorage.setItem("_t1", token); // accessToken
-		const response = await fetch(`/api/user`,
-		    {
-			method: "GET",
-			headers: {"Authorization": `Bearer ${token}`},
-		    }
-		);
-		if (response.status === 200) {
-		    user = await response.json();
-		    console.log(`User data after resfresh: ${JSON.stringify(user, null, 2)}`);
-		    if (user) populateUI(user); else showPopUpMessage("User has been deleted");
-		}
-		if (response.status === 500) showPopUpMessage("Internal server error");
-	    }
-	    if (refreshResponse.status === 401) window.location.replace("/login");
+	  user = await response.json();
+	  console.log(`User data after resfresh: ${JSON.stringify(user, null, 2)}`);
+	  if (user) populateUI(user); else showPopUpMessage("User has been deleted");
 	}
 	if (response.status === 500) showPopUpMessage("Internal server error");
-    } catch (error) {
-	showPopUpMessage("Internal server error");
+      }
+      if (refreshResponse.status === 401) window.location.replace("/login");
     }
+    if (response.status === 500) showPopUpMessage("Internal server error");
+  } catch (error) {
+    showPopUpMessage("Internal server error");
+  }
 });
 
 shareBtn?.addEventListener("click", async () => {
-    try {
-	await navigator.clipboard.writeText(`${url}/${user.userId}`);
-	showPopUpMessage("Link copied", "msg");
-    } catch (error) {
-	showPopUpMessage(error.message)
-    }
+  try {
+    await navigator.clipboard.writeText(`${url}/${user.userId}`);
+    showPopUpMessage("Link copied", "msg");
+  } catch (error) {
+    showPopUpMessage(error.message)
+  }
 });
 
 // ************************* 2. Functions *******************************//
@@ -76,24 +72,24 @@ shareBtn?.addEventListener("click", async () => {
  * @param {User} data
  */
 function populateUI(data) {
-    const previewCard  = document.querySelector('.preview-card');
-    if (previewCard) {
-	const avatar    = previewCard.querySelector('img');
-	const cardName  = previewCard.querySelectorAll('p')[0];
-	const cardEmail = previewCard.querySelectorAll('p')[1];
-	const cardLinks = previewCard.querySelector('ul');
-	if (avatar && cardName && cardEmail && cardLinks) {
-	    if (data.avatar) {
-		avatar.src = data.avatar;
-		avatar.setAttribute('style', 'object-fit: cover;');
-	    } else {
-		avatar.setAttribute('style', 'object-fit: scale-down;');
-	    }
-	    cardName.textContent  = data.name;
-	    cardEmail.textContent = data.email ? data.email : '***********';
-	    cardLinks.innerHTML   = createListOfPreviewLinks(data.links);
-	}
+  const previewCard  = document.querySelector('.preview-card');
+  if (previewCard) {
+    const avatar    = previewCard.querySelector('img');
+    const cardName  = previewCard.querySelectorAll('p')[0];
+    const cardEmail = previewCard.querySelectorAll('p')[1];
+    const cardLinks = previewCard.querySelector('ul');
+    if (avatar && cardName && cardEmail && cardLinks) {
+      if (data.avatar) {
+	avatar.src = data.avatar;
+	avatar.setAttribute('style', 'object-fit: cover;');
+      } else {
+	avatar.setAttribute('style', 'object-fit: scale-down;');
+      }
+      cardName.textContent  = data.name;
+      cardEmail.textContent = data.email ? data.email : '***********';
+      cardLinks.innerHTML   = createListOfPreviewLinks(data.links);
     }
+  }
 }
 
 /**
@@ -101,20 +97,20 @@ function populateUI(data) {
  * @param {Link[]} links
  */
 function createListOfPreviewLinks(links) {
-    let list = '';
-    links.forEach((item) => {
-	const { domain, iconPath, bgColor } = getLinkInfoByName(item.source);
-	const li = `<li>
-                      <a
-                        style="--image_path: url(${iconPath});"
-                        target="_blank"
-                        class=${bgColor}
-                        href=${domain}${item.url}>
-                          ${item.source}
-                      </a>
-                    </li>`;
-	list = list + li;
-    });
-    return list;
+  let list = "";
+  links.forEach((item) => {
+    const params = getParams(new URL(item.url).hostname);
+    const li = `<li>
+		  <a style="--image_path: url('/client/images/icons/${params?.whiteIcon}');"
+		     target="_blank"
+		     class="${params?.bgColor}"
+		     href="${item.url}">
+		    ${params?.title}
+		  </a>
+		</li>`;
+    list = list + li;
+  });
+  
+  return list;
 }
 
--- guru/linksharing-app-cs/client/pages/profile/profile.js
+++ guru/linksharing-app-cs/client/pages/profile/profile.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { getLinkInfoByName, showPopUpMessage } from "../../utils/utils.js";
+import { getParams, showPopUpMessage } from "../../utils/utils.js";
 
 /**
  * @typedef { import("../../../server/src/types/typedefs.js").User } User
@@ -10,20 +10,20 @@ import { getLinkInfoByName, showPopUpMessage } from "../../utils/utils.js";
 // ************************** 1. Events *********************************//
 
 window.addEventListener('load', async () => {
-    try {
-	const response = await fetch(
-	    `/api/user/${window.location.pathname.split("/")[1]}`,
-	);
-	if (response.status === 200) {
-	    const user = await response.json();
-	    console.log(`User data: ${JSON.stringify(user, null, 2)}`);
-	    if (user) populateUI(user); else location.replace("/404");
-	}
-	if (response.status === 400) location.replace("/404");
-	if (response.status === 500) showPopUpMessage("Internal server error");
-    } catch (error) {
-	showPopUpMessage("Internal server error");
+  try {
+    const response = await fetch(
+      `/api/user/${window.location.pathname.split("/")[1]}`,
+    );
+    if (response.status === 200) {
+      const user = await response.json();
+      console.log(`User data: ${JSON.stringify(user, null, 2)}`);
+      if (user) populateUI(user); else location.replace("/404");
     }
+    if (response.status === 400) location.replace("/404");
+    if (response.status === 500) showPopUpMessage("Internal server error");
+  } catch (error) {
+    showPopUpMessage("Internal server error");
+  }
 });
 
 // ************************* 2. Functions *******************************//
@@ -32,24 +32,24 @@ window.addEventListener('load', async () => {
  * @param {User} data
  */
 function populateUI(data) {
-    const previewCard  = document.querySelector('.preview-card');
-    if (previewCard) {
-	const avatar    = previewCard.querySelector('img');
-	const cardName  = previewCard.querySelectorAll('p')[0];
-	const cardEmail = previewCard.querySelectorAll('p')[1];
-	const cardLinks = previewCard.querySelector('ul');
-	if (avatar && cardName && cardEmail && cardLinks) {
-	    if (data.avatar) {
-		avatar.src = data.avatar;
-		avatar.setAttribute('style', 'object-fit: cover;');
-	    } else {
-		avatar.setAttribute('style', 'object-fit: scale-down;');
-	    }
-	    cardName.textContent  = data.name;
-	    cardEmail.textContent = data.email ? data.email : '***********';
-	    cardLinks.innerHTML   = createListOfPreviewLinks(data.links);
-	}
+  const previewCard  = document.querySelector('.preview-card');
+  if (previewCard) {
+    const avatar    = previewCard.querySelector('img');
+    const cardName  = previewCard.querySelectorAll('p')[0];
+    const cardEmail = previewCard.querySelectorAll('p')[1];
+    const cardLinks = previewCard.querySelector('ul');
+    if (avatar && cardName && cardEmail && cardLinks) {
+      if (data.avatar) {
+	avatar.src = data.avatar;
+	avatar.setAttribute('style', 'object-fit: cover;');
+      } else {
+	avatar.setAttribute('style', 'object-fit: scale-down;');
+      }
+      cardName.textContent  = data.name;
+      cardEmail.textContent = data.email ? data.email : '***********';
+      cardLinks.innerHTML   = createListOfPreviewLinks(data.links);
     }
+  }
 }
 
 /**
@@ -57,20 +57,19 @@ function populateUI(data) {
  * @param {Link[]} links
  */
 function createListOfPreviewLinks(links) {
-    let list = '';
-    links.forEach((item) => {		
-	const { domain, iconPath, bgColor } = getLinkInfoByName(item.source);
-	const li = `<li>
-                      <a
-                        style="--image_path: url(${iconPath});"
-                        target="_blank"
-                        class=${bgColor}
-                        href=${domain}${item.url}>
-                          ${item.source}
-                      </a>
-                    </li>`;
-	list = list + li;
-    });
-    return list;
+  let list = "";
+  links.forEach((item) => {		
+    const params = getParams(new URL(item.url).hostname);
+    const li = `<li>
+		  <a style="--image_path: url('/client/images/icons/${params?.whiteIcon}');"
+		     target="_blank"
+		     class=${params?.bgColor}
+		     href=${item.url}>
+		    ${params?.title}
+		  </a>
+		</li>`;
+    list = list + li;
+  });
+  return list;
 }
 
