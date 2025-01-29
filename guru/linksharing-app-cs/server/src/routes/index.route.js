31e0f0b83b67f75f5925dadf1f6fc73de31d421e
AlexKolykhalov <ak8647@rambler.ru>
2025-01-29 08:54:10 +0300

    guru:02:c-s Removed console.log

--- guru/linksharing-app-cs/server/src/routes/index.route.js
+++ guru/linksharing-app-cs/server/src/routes/index.route.js
@@ -8,9 +8,7 @@ const root = process.env.NODE_ENV === "development" ?
       path.resolve("..", "client"):
       path.resolve("guru", "linksharing-app-cs", "client");
 
-console.log(`my root path: ${root}`);
-
-const router    = Router();
+const router = Router();
 
 router.use("/api", authRouter);
 router.use("/api", cronRouter);
