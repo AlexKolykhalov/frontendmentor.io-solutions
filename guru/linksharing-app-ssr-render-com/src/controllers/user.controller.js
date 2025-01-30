78b1fc33e8895cc0a7b6469954b6d21c677dc9da
AlexKolykhalov <ak8647@rambler.ru>
2025-01-30 16:18:55 +0300

    guru:04:ssr Changed path of templates

--- guru/linksharing-app-ssr-render-com/src/controllers/user.controller.js
+++ guru/linksharing-app-ssr-render-com/src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import express from "express";
 import { UserService } from "../services/user.service.js";
 import { ServerError } from "../errors/server.error.js";
 import { getCompileEJS } from "../utils.js";
-import { getLinkInfoByName } from "../../public/helpers.js";
+import { getParams } from "../../public/helpers.js";
 
 /**
  * @class
@@ -34,25 +34,21 @@ export class UserController {
     try {
       const user = await UserService.findByUserToken(req.cookies["_t2"]);
       if (!user) throw ServerError.notFound();
-      const links = user.links.map(item => {
-	const { domain, whiteIcon, grayIcon, bgColor, offset } = getLinkInfoByName(item.source);
+      user.links = user.links.map((item) => {
 	return {
-	  id: item.linkId,
-	  domain: domain,
-	  whiteIcon: whiteIcon,
-	  grayIcon: grayIcon,
-	  bgColor: bgColor,
-	  offset: offset,
+	  id: item.id,
+	  number: user.links.indexOf(item) + 1,
 	  url: item.url,
-	  source: item.source };
-      });      
-      const compiled = getCompileEJS("/public/templates/index.ejs");
-      const html = compiled({ user: user, links: links });
+	  params: getParams(new URL(item.url).hostname) };
+      });
+
+      const compiled = getCompileEJS("/src/views/index.ejs");
+      const html     = compiled({ user: user });      
       return res.format({
 	json () { return res.json(user) },
 	html () { return res.send(html) },
       });
-    } catch (error) {      
+    } catch (error) {
       next(error);
     }
   }
@@ -66,16 +62,16 @@ export class UserController {
     try {
       const user = await UserService.findByUserId(req.params.userId);
       if (!user) throw ServerError.notFound();
-      const links = user.links.map(item => {
-	const { domain, whiteIcon, bgColor } = getLinkInfoByName(item.source);
-	return { domain: domain,
-		 whiteIcon: whiteIcon,
-		 bgColor: bgColor,
-		 url: item.url,
-		 source: item.source };
+      user.links = user.links.map(item => {
+	return {
+	  id: item.id,
+	  number: user.links.indexOf(item) + 1,
+	  url: item.url,
+	  params: getParams(new URL(item.url).hostname) };
       });
-      const compiled = getCompileEJS("/public/templates/profile.ejs");
-      const html     = compiled({ user: user, userLinks: links });
+      
+      const compiled = getCompileEJS("/src/views/profile.ejs");
+      const html     = compiled({ user: user });
       return res.format({
 	json () { return res.json(user) },
 	html () { return res.send(html) },
@@ -94,16 +90,16 @@ export class UserController {
     try {
       const user = await UserService.findByUserToken(req.cookies["_t2"]);
       if (!user) throw ServerError.notFound();
-      const links = user.links.map(item => {
-	const { domain, whiteIcon, bgColor } = getLinkInfoByName(item.source);
-	return { domain: domain,
-		 whiteIcon: whiteIcon,
-		 bgColor: bgColor,
-		 url: item.url,
-		 source: item.source };
+      user.links = user.links.map(item => {
+	return {
+	  id: item.id,
+	  number: user.links.indexOf(item) + 1,
+	  url: item.url,
+	  params: getParams(new URL(item.url).hostname) };
       });
-      const compiled = getCompileEJS("/public/templates/preview.ejs");
-      const html     = compiled({ user: user, userLinks: links });
+
+      const compiled = getCompileEJS("/src/views/preview.ejs");
+      const html     = compiled({ user: user });
       return res.format({
 	json () { return res.json(user) },
 	html () { return res.send(html) },
