690abad7b38b2dcbdf817af823819b260b5e7d95
AlexKolykhalov <ak8647@rambler.ru>
2025-01-30 16:26:55 +0300

    guru:04:ssr Removed "source" field, updated "linkId" to "id"

--- guru/linksharing-app-ssr-render-com/src/services/link.service.js
+++ guru/linksharing-app-ssr-render-com/src/services/link.service.js
@@ -10,55 +10,54 @@ import db from "../database/models/index.js";
  * @class
  */
 export class LinkService {
-    /**
-     * @async
-     * @param {string} linkId
-     * @returns {Promise<Link|null>}
-     */
-    static async findLink(linkId) {
-	return await db.Link.findOne({
-	    attributes: ["link_id", "source", "url"],
-	    where: { linkId: linkId }
-	});
-    }
+  /**
+   * @async
+   * @param {string} linkId
+   * @returns {Promise<Link|null>}
+   */
+  static async findLink(linkId) {
+    return await db.Link.findOne({
+      attributes: ["link_id", "url"],
+      where: { linkId: linkId }
+    });
+  }
 
-    /**
-     * @async
-     * @param {Link} link
-     * @returns {Promise<Link>}
-     */
-    static async updateLink(link) {
-	return await db.Link.update(
-	    link,
-	    { where: { linkId: link.linkId } }
-	);
-    }
+  /**
+   * @async
+   * @param {Link} link
+   * @returns {Promise<Link>}
+   */
+  static async updateLink(link) {
+    return await db.Link.update(
+      { link_id: link.id, url: link.url },
+      { where: { linkId: link.id } }
+    );
+  }
 
-    /**
-     * @async
-     * @param {Link} link
-     * @param {string} userId
-     * @returns {Promise<Link>}
-     */
-    static async createLink(link, userId) {
-	return await db.Link.create(
-	    {
-		source: link.source,
-		url: link.url,
-		linkId: link.linkId,		
-		userId: userId
-	    }
-	);
-    }
+  /**
+   * @async
+   * @param {Link} link
+   * @param {string} userId
+   * @returns {Promise<Link>}
+   */
+  static async createLink(link, userId) {
+    return await db.Link.create(
+      {
+	url: link.url,
+	linkId: link.id,
+	userId: userId
+      }
+    );
+  }
 
-    /**
-     * @async
-     * @param {Link} link
-     * @returns {Promise<void>}
-     */
-    static async deleteLink(link) {
-	return await db.Link.destroy({
-	    where: { linkId: link.linkId }
-	});
-    }
+  /**
+   * @async
+   * @param {Link} link
+   * @returns {Promise<void>}
+   */
+  static async deleteLink(link) {
+    return await db.Link.destroy({
+      where: { linkId: link.id }
+    });
+  }
 }
--- guru/linksharing-app-ssr-render-com/src/services/user.service.js
+++ guru/linksharing-app-ssr-render-com/src/services/user.service.js
@@ -25,12 +25,12 @@ export class UserService {
 	{
 	  model: db.Link,
 	  as: "links",
-	  attributes: ["linkId", "source", "url"],
+	  attributes: [["link_id", "id"], "url"],
 	  required: false,
 	},
       ],
       where: { userId: userId },
-      order: [ [{model: db.Link, as: "links"}, "source", "ASC"] ]
+      order: [ [{model: db.Link, as: "links"}, "id", "ASC"] ]
     });
   }
 
@@ -61,10 +61,10 @@ export class UserService {
       );
       if (count === 0) throw new Error("Can't update a User");
       for (const item of userData.links) {
-	const link = await LinkService.findLink(item.linkId);
-	if (!link)                             await LinkService.createLink(item, userData.userId);
-	if (link && (item.source || item.url)) await LinkService.updateLink(item);
-	if (link && !item.source && !item.url) await LinkService.deleteLink(item);
+	const link = await LinkService.findLink(item.id);
+	if (!link)             await LinkService.createLink(item, userData.userId);
+	if (link && item.url)  await LinkService.updateLink(item);
+	if (link && !item.url) await LinkService.deleteLink(item);
       }
     });
     return await this.findByUserId(userData.userId);
