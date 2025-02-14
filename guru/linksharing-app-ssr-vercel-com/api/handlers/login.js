77e6c554521fa7a5fc6e2f95beb2d5e9255e0856
AlexKolykhalov <ak8647@rambler.ru>
2025-02-14 15:25:23 +0300

    guru:03:ssr(vercel) Added "views" option

--- guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
+++ guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
@@ -1,18 +1,21 @@
 import ejs  from "ejs";
 import path from "path";
-import { readFileSync } from "fs";
+// import { readFileSync } from "fs";
 
 export default function (_, res) {
-  // ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { views: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
-  //   if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
-  //   res.send(str);
-  // });
-  console.log(`my path: ${path.resolve()}/public/pages/login/login.ejs`);
-  const compile =  ejs.compile(
-    readFileSync(`${path.resolve()}/public/pages/login/login.ejs`, "utf-8"),
-    { views: [path.resolve()+"/public/pages/login", path.resolve()+"/public/pages"] }
+  ejs.renderFile(
+    `${path.resolve()}/public/pages/login/login.ejs`,
+    { views: [`${path.resolve()}/public/pages`] },
+    (err, str) => {
+      if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
+      res.send(str);
+    }
   );
-
-
-  res.send(compile());
+  
+  // console.log(`my path: ${path.resolve()}/public/pages/login/login.ejs`);
+  // const compile =  ejs.compile(
+  //   readFileSync(`${path.resolve()}/public/pages/login/login.ejs`, "utf-8"),
+  //   { views: [path.resolve()+"/public/pages/login", path.resolve()+"/public/pages"] }
+  // );
+  // res.send(compile());
 }
