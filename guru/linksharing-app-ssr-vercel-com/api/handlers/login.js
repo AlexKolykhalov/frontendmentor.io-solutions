4f455193e86a96f36bfd0a7d5eac800b1a34721e
AlexKolykhalov <ak8647@rambler.ru>
2025-02-14 14:20:05 +0300

    guru:03:ssr(vercel) Added "filename" option

--- guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
+++ guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
@@ -2,7 +2,7 @@ import ejs  from "ejs";
 import path from "path";
 
 export default function (_, res) {
-  ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, (err, str) => {
+  ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { filename: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
     if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
     res.send(str);
   });
