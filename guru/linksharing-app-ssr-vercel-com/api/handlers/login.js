ecca78080a549998b6bdbf3a9a9e0b1832f245c2
AlexKolykhalov <ak8647@rambler.ru>
2025-02-14 14:29:20 +0300

    guru:03:ssr(vercel) Changed code logic

--- guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
+++ guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
@@ -1,9 +1,18 @@
 import ejs  from "ejs";
 import path from "path";
+import { readFileSync } from "fs";
 
 export default function (_, res) {
-  ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { filename: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
-    if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
-    res.send(str);
-  });
+  // ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { filename: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
+  //   if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
+  //   res.send(str);
+  // });
+  console.log(`my path: ${path.resolve()}/public/pages/login/login.ejs`);
+  const compile =  ejs.compile(
+    readFileSync(`${path.resolve()}/public/pages/login/login.ejs`, "utf-8"),
+    { filename: `${path.resolve()}/public/pages/login/login.ejs` }
+  );
+
+
+  res.send(compile());
 }
