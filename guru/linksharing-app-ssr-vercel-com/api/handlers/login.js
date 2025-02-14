df7726fadf4d344f5905a70a0907a1d80f1dc8b1
AlexKolykhalov <ak8647@rambler.ru>
2025-02-14 14:52:36 +0300

    guru:03:ssr(vercel) Changed path of ejs file

--- guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
+++ guru/linksharing-app-ssr-vercel-com/api/handlers/login.js
@@ -1,9 +1,9 @@
 import ejs  from "ejs";
 import path from "path";
-import { readFileSync } from "fs";
+// import { readFileSync } from "fs";
 
 export default function (_, res) {
-  // ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { filename: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
+  // ejs.renderFile(`${path.resolve()}/public/pages/login/login.ejs`, { views: `${path.resolve()}/public/pages/login/login.ejs` }, (err, str) => {
   //   if (err) return res.status(500).json({ error: "EJS render error", text: err.message });
   //   res.send(str);
   // });
