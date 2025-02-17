7b07fe0e0fa50f07997df61cafaa69eb8efb4dc4
AlexKolykhalov <ak8647@rambler.ru>
2025-02-17 14:22:34 +0300

    guru:03:ssr(vercel) Shortened some lines of code

--- guru/linksharing-app-ssr-vercel-com/db.js
+++ guru/linksharing-app-ssr-vercel-com/db.js
@@ -1,8 +1,3 @@
 import postgres from "postgres";
 
-const connectionString = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:6543/postgres`;
-
-const sql = postgres(connectionString);
-
-export default sql;
-
+export default postgres(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:6543/postgres`);
