dddbd904826d01b612ccb101d1f382095249ca8a
AlexKolykhalov <ak8647@rambler.ru>
2025-08-14 16:37:16 +0300

    guru:kanban-ssr-vercel Removed SQL function

--- guru/kanban-task-management-ssr-vercel-com/src/db/functions/delete_task.sql
+++
@@ -1,7 +0,0 @@
-create function api.delete_task(p_id uuid) returns void as
-$$
-begin
-  with delete_task as (delete from api.tasks where task_id = p_id returning task_id)
-  delete from api.subtasks where task_id in (select task_id from delete_task);
-end;
-$$ language plpgsql;
