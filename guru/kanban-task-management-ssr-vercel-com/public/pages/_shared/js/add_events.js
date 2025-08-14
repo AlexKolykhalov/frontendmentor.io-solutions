19d5d832bdbe87c6e69deac35ce3f59e5ba14e73
AlexKolykhalov <ak8647@rambler.ru>
2025-08-14 16:43:12 +0300

    guru:kanban-ssr-vercel Rename files
    Moved load.js from /pages/index/js/load.js to /pages/_shared/js/load.js
    Removed pages/index/js/inject.js
    Created pages/_shared/js/add_events.js

---
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/_shared/js/add_events.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/** Adds events into ssr components. */
+export function addEvents() {
+  if (!document.body.dataset.paths) throw new Error("Missing <body> [data-paths]");
+  document.body.dataset.paths.split(";").forEach(async (path) => {
+    const module       = await import(path);
+    const className    = Object.keys(module)[0];
+    const DynamicClass = module[className];
+    document.querySelectorAll(DynamicClass.selector).forEach(element => {
+      DynamicClass.handleEvents(element);
+      console.log(`add ${className} events`);
+    });
+  });
+  delete document.body.dataset.paths;
+}
