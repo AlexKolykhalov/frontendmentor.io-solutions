34d23f88b36623f9eb6018bce800279d110a6616
AlexKolykhalov <ak8647@rambler.ru>
2025-08-15 10:13:03 +0300

    guru:kanban-ssr-vercel Optimazed component
    1. Using "this" where possible.
    2. Added SSR attributes (path, type of static functions, etc.).
    3. Take into the Role when adding BoardsListItem.

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/boards_list.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/boards_list.js
@@ -1,30 +1,32 @@
 // @ts-check
 
+import { Role, roles }    from "../../_shared/roles.js";
 import { BoardsListItem } from "./boards_list_item.js";
-import { insert }         from "./helpers.js";
+import { insert }         from "./_helpers.js";
 
+// listens to [board:selected, created, updated, deleted]
 export class BoardsList {
 
-  static prefix = "boards_list";
+  static prefix     = "boards_list"; // add_new_board_dialog.js
+  static selector   = `#${this.prefix}`;
 
-  /**
-   * @param {import("./board.js").BoardType[]} boards
+  /**   
+   * @param {import("./board.js").BoardType[]} props
+   *
    * @returns {string} HTML string
    */
-  static template(boards) {
-    const boardsListItems = boards.map(
-      (board, index) => BoardsListItem.template(
-	{
-	  id:       board.id,
-	  title:    board.name,
-	  selected: index === 0
-	}
-      )
-    ).join("");
+  static template(props) {
+    const path           = `data-path="http://localhost:3000/pages/index/components/boards_list.js"`;
+    const boardsListItem = 
+	  props.map((board, index) => BoardsListItem.template(
+	    { id: board.id, title: board.name, selected: index === 0, locked: index === 1 },
+	    { SSR: true }
+	  ))
+	  .join("");
 
-    return `<article id="${this.prefix}">
-              <header class="fs-300 fw-bold clr-n-600 letter-spacing-m pad-left-l pad-v-m">ALL BOARDS (${boards.length})</header>
-              <ul>${boardsListItems}</ul>
+    return `<article id="${this.prefix}" ${path}>
+              <header class="fs-300 fw-bold clr-n-600 letter-spacing-m pad-left-l pad-v-m">ALL BOARDS (${props.length})</header>
+              <ul>${boardsListItem}</ul>
               <br>
               <button class="create-new-board-btn { transparent row gap-m cross-axis-center fw-bold pad-v-m clr-p-purple }"><img src="images/svg/icon-board-purple.svg" alt="" width="16" height="16">+ Create New Board</button>
               <button class="[ md:display-none ] add-new-task-btn { transparent row gap-m cross-axis-center fw-bold pad-v-m clr-p-purple }"><img src="images/svg/icon-board-purple.svg" alt="" width="16" height="16">+ Add New Task</button>
@@ -32,37 +34,15 @@ export class BoardsList {
   }
 
   /**
-   * @param {import("./board.js").BoardType[]} boards
-   * @returns {Element}
-   */
-  static init(boards) {
-    return BoardsList.#create(boards);
-  }
-
-  /**
-   * @param {import("./board.js").BoardType[]} boards
-   * @returns {Element}
-   */
-  static #create(boards) {
-    const template     = document.createElement("template");
-    template.innerHTML = BoardsList.template(boards);
-    const component    = template.content.firstElementChild;
-    if (!component) throw new Error("Can't create \"BoardsList\" component");
-
-    BoardsList.handleEvents(component);
-
-    return component;
-  }
-
-  /**
    * @param {Element} component
+   *
    * @returns {void}
    */
   static handleEvents(component) {
     // btn "Create New Board"
     component.querySelector(".create-new-board-btn")?.addEventListener("click", async () => {
-      const { CreateNewBoardDialog } = await import("../components/add_new_board_dialog.js");
-      const dialog = CreateNewBoardDialog.init();
+      const { AddNewBoardDialog } = await import("../components/add_new_board_dialog.js");
+      const dialog = AddNewBoardDialog.init();
       document.querySelector("body")?.appendChild(dialog);
       // @ts-ignore
       dialog.showModal();
@@ -85,18 +65,25 @@ export class BoardsList {
 
       header.textContent = `ALL BOARDS (${[...ul.children].length + 1})`;
       ul.querySelector("li.selected")?.classList.remove("selected");
-      ul.appendChild(
-	// @ts-ignore
-        BoardsListItem.init({ id: event.detail.id, title: event.detail.name, selected: true })
+      // @ts-ignore
+      const data = event.detail;
+      ul.appendChild(	
+	BoardsListItem.init({
+	  id:       data.id,
+	  title:    data.name,
+	  selected: true,
+	  locked:   Role.getRole() === roles.ANONYMOUS
+	})
       );
 
       console.log("board:created");
     });
 
     component.addEventListener("board:updated", (event) => {
-      insert(
-	// @ts-ignore
-        BoardsListItem.init({id: event.detail.id, title: event.detail.name, selected: true}),
+      // @ts-ignore
+      const data = event.detail;
+      insert(	
+	BoardsListItem.init({ id: data.id, title: data.name, selected: true }),
         "ul > li.selected",
         component
       );
