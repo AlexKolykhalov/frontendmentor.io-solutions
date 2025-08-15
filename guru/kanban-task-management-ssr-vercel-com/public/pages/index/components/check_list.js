f880fc97dd9890e7e7c0f5a72e81eb3cf24e104b
AlexKolykhalov <ak8647@rambler.ru>
2025-08-15 10:30:21 +0300

    guru:kanban-ssr-vercel Optimazed component
    1. Using "this" where possible.
    2. Added CSR attributes (type of static functions, etc.).

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/check_list.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/check_list.js
@@ -1,29 +1,31 @@
 // @ts-check
 
-import { injectEvents }  from "../js/inject.js";
+import { generateRandomSymbols, insert } from "./_helpers.js";
 import { CheckListItem } from "./check_list_item.js";
 
 /**
  * @typedef {object} CheckListType
- * @property {string} [title] Title of the check list
- * @property {import("./check_list_item").CheckListItemType[]} items
+ * @property {string} [title] By default "Check list".
+ * @property {import("./check_list_item.js").CheckListItemType[]} [items] By default [].
  */
 
+// listens to [check-list-item:changed]
 export class CheckList {
+
   /**
    * @param {CheckListType} [props]
    *
-   * @returns {string} - HTML string
+   * @returns {string} HTML string
    */
-  static template(props = { items: [] }) {
-    const { title = "Checkbox list", items } = props;
-    return `<div class="column gap-sm">
+  static #template(props) {
+    const title = props ? props.title ?? "Check list" : "Check list";
+    const items = props ? props.items ?? [] : [];
+
+    // An ID required if there are two or more "checklists" in one parent component
+    return `<div id="${generateRandomSymbols(4)}" class="column gap-sm">
               <p class="fw-bold fs-200 clr-n-600 letter-spacing-m">${title} (${items.filter(item => item.checked).length} of ${items.length})</p>
               <ul class="column gap-sm">
-                ${items.map(item => CheckListItem.template({
-                  id: item.id,
-                  title: item.title,
-                  checked: item.checked})).join("")}
+                <check-list-items></check-list-items>
               </ul>
             </div>`;
   }
@@ -33,44 +35,54 @@ export class CheckList {
    *
    * @returns {Element}
    */
-  static init(props = { items: [] }) {
-    const component = CheckList.#create(props);
-    injectEvents(component); // adds events for children (CheckListItem)
-    
+  static init(props) {
+    const component = this.#create(props);
+
+    const fragment = document.createDocumentFragment();
+    if (props) {
+      props.items?.forEach(
+	item => {
+	  fragment.appendChild(
+	    CheckListItem.init({ id: item.id, value: item.value, checked: item.checked })
+	  );
+	}
+      );
+    }
+    insert(fragment, "check-list-items", component);
+
     return component;
   }
 
   /**
-   * @param {CheckListType} props
+   * @param {CheckListType} [props]
    *
    * @returns {Element}
    */
   static #create(props) {
     const template     = document.createElement("template");
-    template.innerHTML = CheckList.template(props);
+    template.innerHTML = this.#template(props);
     const component    = template.content.firstElementChild;
     if (!component)    throw new Error("Can't create \"CheckList\" component");
 
-    CheckList.handleEvents(component, props);
+    this.#handleEvents(component);
 
     return component;
   }
 
   /**
    * @param {Element} component
-   * @param {CheckListType} [props]
    *
    * @returns {void}
    */
-  static handleEvents(component, props = { items: [] }) {
-    const { title, items } = props;
-
-    component.addEventListener("check-list-item:change", () => {
+  static #handleEvents(component) {
+    component.addEventListener("check-list-item:changed", () => {
       const titleElement = component.querySelector("p");
       if (!titleElement) throw new Error("Missing <p>");
-      
-      const checked = [...component.querySelectorAll("input")].filter(input => input.checked);
-      titleElement.textContent=`${title} (${checked.length} of ${items.length})`;
+      const title = titleElement.textContent?.slice(0, titleElement.textContent.lastIndexOf("(")).trim();
+
+      const allCheckboxes     = [...component.querySelectorAll("input")];
+      const checkedCheckboxes = allCheckboxes.filter(input => input.checked);
+      titleElement.textContent=`${title} (${checkedCheckboxes.length} of ${allCheckboxes.length})`;
     });
   }
 }
