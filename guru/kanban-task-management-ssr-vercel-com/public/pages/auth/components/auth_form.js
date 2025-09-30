7fbea3d7797ec03eed5ccdbd42b7e61003281726
AlexKolykhalov <ak8647@rambler.ru>
2025-09-30 16:41:05 +0300

    guru:kanban-ssr-vercel Optimazed the component
    1. Removed some static values
    2. Changed status code from 201 to 200
    3. Using a popup window instead of throwing an Error to the console

--- guru/kanban-task-management-ssr-vercel-com/public/pages/auth/components/auth_form.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/auth/components/auth_form.js
@@ -1,26 +1,23 @@
 // @ts-check
 
-import { LoaderRipple } from "../../_shared/components/loader-ripple.js";
-import { PopUp }        from "../../_shared/components/pop-up.js";
-
 export class AuthForm {
 
-  static prefix     = "auth_form";
-  static custom_tag = "auth-form";
-  static selector   = `#${this.prefix}`;
-  static #path      = "http://localhost:3000/pages/auth/components/auth_form.js";
+  static prefix   = "auth_form";
+  static selector = `#${this.prefix}`;
 
   /** @returns {string} HTML string */
   static template() {
-    return `<div id="${this.prefix}" class="column gap-l" data-path="${this.#path}">
+    const path = `data-path="http://localhost:3000/pages/auth/components/auth_form.js"`;
+
+    return `<div id="${this.prefix}" class="column gap-l" ${path}>
 	      <div class="column gap-m">
 	        <div class="column gap-sm">
 	          <label class="clr-n-600-000" for="login">Login</label>
 	          <input class="pad-sm clr-n-900-000 bg-n-100-900" id="login" maxlength="50">
 	        </div>
 	        <div class="column gap-sm">
-	          <label class="clr-n-600-000" for="password">Password</label>
-	          <input class="pad-sm clr-n-900-000 bg-n-100-900" id="password" type="password" minlength="6">
+	          <label class="clr-n-600-000" for="password">Password (more than 5 symbols)</label>
+	          <input class="pad-sm clr-n-900-000 bg-n-100-900" id="password" type="password">
 	        </div>
 	      </div>
 	      <div class="column gap-m">
@@ -39,49 +36,166 @@ export class AuthForm {
   static handleEvents(component) {
     const contrsBtn = component.querySelectorAll("button");
 
+    /** @type {HTMLInputElement|null} */
+    const inputLogin = component.querySelector("#login");
+    if (!inputLogin) throw new Error(`Missing <input id="login">`);
+    inputLogin.addEventListener("input", function () { this.removeAttribute("style") } );
+
+    /** @type {HTMLInputElement|null} */
+    const inputPassword = component.querySelector("#password");
+    if (!inputPassword) throw new Error(`Missing <input id="password">`);
+    inputPassword.addEventListener("input", function () { this.removeAttribute("style") } );
+
     // sign in (anonymously)
     contrsBtn[0].addEventListener("click", async function() {
       this.setAttribute("disabled", "");
+      // add indicator
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+      const loader = LoaderRipple.init();
+      loader.classList.add("clr-n-000");
+      loader.setAttribute("style", "--size: 25px; right: 5%;");
+      this.appendChild(loader);
+
+      // [Errors 400, 401, 500] [Success 200]
+      const response = await fetch(`http://localhost:3000/api/signin`, { method: "POST" });
+      if (response.status !== 200) {
+	this.removeAttribute("disabled");
+	loader.remove();
+
+	const { PopUp } = await import("../../_shared/components/pop_up.js");
+	document.body.appendChild(
+	  PopUp.init({
+	    title: response.status === 401 ? "Unauthorized" : "Server error",
+	    message: response.status === 401 ?
+	      "Invalid credentials." :
+	      "Something went wrong. Try again."
+	  })
+	);
+
+	return;
+      }
+      localStorage.setItem("bearer", await response.json());
+      window.location.replace("/"); // redirect to index.html
+    });
+
+    // sign in (login & password)
+    contrsBtn[1].addEventListener("click", async function() {
+
+      if (!validation()) return;
+
+      this.setAttribute("disabled", "");
+      // add indicator
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+      const loader = LoaderRipple.init();
+      loader.classList.add("clr-n-000");
+      loader.setAttribute("style", "--size: 25px; right: 5%;");
+      this.appendChild(loader);
+
+      // [Errors 400, 401, 500] [Success 200]
+      const response = await fetch(
+	`http://localhost:3000/api/signin`,
+	{
+	  method: "POST",
+	  headers: { "Content-Type": "application/json" },
+	  body: JSON.stringify({
+	    login:    inputLogin.value.trim(),
+	    password: inputPassword.value.trim(),
+	  })
+	}
+      );
+
+      if (response.status !== 200) {
+	this.removeAttribute("disabled");
+	loader.remove();
+
+	const { PopUp } = await import("../../_shared/components/pop_up.js");
+	document.body.appendChild(
+	  PopUp.init({
+	    title: response.status === 401 ? "Unauthorized" : "Server error",
+	    message: response.status === 401 ?
+	      "Invalid credentials." :
+	      "Something went wrong. Try again."
+	  })
+	);
+
+	return;
+      }
+
+      localStorage.setItem("bearer", await response.json());
+      window.location.replace("/"); // redirect to index.html
+    });
+
+    // sign up
+    contrsBtn[2].addEventListener("click", async function() {
+      
+      if (!validation()) return;
+
+      this.setAttribute("disabled", "");
+      // add indicator
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
       const loader = LoaderRipple.init();
       loader.classList.add("clr-n-000");
       loader.setAttribute("style", "--size: 25px; right: 5%;");
       this.appendChild(loader);
 
       // [Errors 400, 401, 500] [Success 201]
-      const response = await fetch(`http://localhost:3000/api/signin`, { method: "POST" });
-      if (response.status === 401) throw new Error("Invalid credentials");
+      const response = await fetch(
+	`http://localhost:3000/api/signup`,
+	{
+	  method: "POST",
+	  headers: { "Content-Type": "application/json" },
+	  body: JSON.stringify({
+	    login:    inputLogin.value.trim(),
+	    password: inputPassword.value.trim(),
+	  })
+	}
+      );
+
       if (response.status !== 201) {
 	this.removeAttribute("disabled");
-	loader.remove();	
+	loader.remove();
+
+	const { PopUp } = await import("../../_shared/components/pop_up.js");
 	document.body.appendChild(
-	  PopUp.init({ title: "Server error", message: "Something went wrong. Try again." })
+	  PopUp.init({
+	    title: response.status === 409 ? "Conflict" : "Server error",
+	    message: response.status === 409 ?
+	      "Login already exists." :
+	      "Something went wrong. Try again."
+	  })
 	);
-	
+
 	return;
       }
-      localStorage.setItem("bearer", await response.json());
-      localStorage.setItem("role", "anonymous");
+
+      localStorage.setItem("bearer", await response.json());      
       window.location.replace("/"); // redirect to index.html
     });
 
-    // sign in (login & password)
-    contrsBtn[1].addEventListener("click", function() {
-      console.log("click Sign in (login & password)");
-      this.setAttribute("disabled", "");
-      const loader = LoaderRipple.init();
-      loader.classList.add("clr-n-000");
-      loader.setAttribute("style", "--size: 25px; right: 27%;");
-      this.appendChild(loader);
-    });
+    // helper functions
 
-    // sign up
-    contrsBtn[2].addEventListener("click", function() {
-      console.log("click Sign up");
-      this.setAttribute("disabled", "");
-      const loader = LoaderRipple.init();
-      loader.classList.add("clr-n-000");
-      loader.setAttribute("style", "--size: 25px; right: 27%;");
-      this.appendChild(loader);
-    });
+    /** @returns {boolean} */
+    function validation() {
+      /** @type {HTMLInputElement|null} */
+      const inputLogin    = component.querySelector("#login");
+      /** @type {HTMLInputElement|null} */
+      const inputPassword = component.querySelector("#password");
+      if (!inputLogin)    throw new Error(`Missing <input id="login">`);
+      if (!inputPassword) throw new Error(`Missing <input id="password">`);
+
+      let isValid = true;
+
+      if (!inputLogin.value.trim()) {
+	inputLogin.setAttribute("style", "border-color: red;");
+	isValid = false;
+      }
+
+      if (!inputPassword.value.trim() || inputPassword.value.trim().length < 6 ) {
+	inputPassword.setAttribute("style", "border-color: red;");
+	isValid = false;
+      }
+
+      return isValid;
+    }
   }
 }
