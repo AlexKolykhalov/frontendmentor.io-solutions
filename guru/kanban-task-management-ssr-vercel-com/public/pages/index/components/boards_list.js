3edde6ef63020be9c84b8d81cc1492311a3fd152
AlexKolykhalov <ak8647@rambler.ru>
2025-09-30 16:56:19 +0300

    guru:kanban-ssr-vercel Optimazed the component
    1. Removed some static values
    2. Implemented click of "Edit board" & "Delete board" buttons in mobile version
    3. Using a "boards-list-btn" instead of "create-new-board-bnt" in class attr
    4. Removed props SSR:true

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/boards_list.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/boards_list.js
@@ -1,35 +1,35 @@
 // @ts-check
 
-import { Role, roles }    from "../../_shared/roles.js";
 import { BoardsListItem } from "./boards_list_item.js";
-import { insert }         from "./_helpers.js";
 
 // listens to [board:selected, created, updated, deleted]
 export class BoardsList {
 
-  static prefix     = "boards_list"; // add_new_board_dialog.js
-  static selector   = `#${this.prefix}`;
+  static prefix   = "boards_list"; // add_new_board_dialog.js
+  static selector = `#${this.prefix}`;
 
-  /**   
+  /**
    * @param {import("./board.js").BoardType[]} props
    *
    * @returns {string} HTML string
    */
   static template(props) {
     const path           = `data-path="http://localhost:3000/pages/index/components/boards_list.js"`;
-    const boardsListItem = 
-	  props.map((board, index) => BoardsListItem.template(
-	    { id: board.id, title: board.name, selected: index === 0, locked: index === 1 },
-	    { SSR: true }
-	  ))
-	  .join("");
+    const boardsListItem = props.map(
+      (board, index) => BoardsListItem.template({
+	id: board.id, title: board.name, selected: index === 0
+      })
+    ).join("");
 
     return `<article id="${this.prefix}" ${path}>
               <header class="fs-300 fw-bold clr-n-600 letter-spacing-m pad-left-l pad-v-m">ALL BOARDS (${props.length})</header>
               <ul>${boardsListItem}</ul>
               <br>
-              <button class="create-new-board-btn { transparent row gap-m cross-axis-center fw-bold pad-v-m clr-p-purple }"><img src="images/svg/icon-board-purple.svg" alt="" width="16" height="16">+ Create New Board</button>
-              <button class="[ md:display-none ] add-new-task-btn { transparent row gap-m cross-axis-center fw-bold pad-v-m clr-p-purple }"><img src="images/svg/icon-board-purple.svg" alt="" width="16" height="16">+ Add New Task</button>
+              <button class="boards-list-btn { row gap-m cross-axis-center }"><img src="images/svg/icon-create.svg" alt="">Create New Board</button>
+              <button class="[ md:display-none ] boards-list-btn { row gap-m cross-axis-center }"><img src="images/svg/icon-edit.svg" alt="">Edit board</button>
+              <button class="[ md:display-none ] boards-list-btn { row gap-m cross-axis-center }"><img src="images/svg/icon-delete.svg" alt="">Delete board</button>
+              <br>
+              <button class="[ md:display-none ] boards-list-btn { row gap-m cross-axis-center }"><img src="images/svg/icon-create.svg" alt="">Add New Task</button>
             </article>`;
   }
 
@@ -39,8 +39,10 @@ export class BoardsList {
    * @returns {void}
    */
   static handleEvents(component) {
+    const btns = component.querySelectorAll("button");
+
     // btn "Create New Board"
-    component.querySelector(".create-new-board-btn")?.addEventListener("click", async () => {
+    btns[0].addEventListener("click", async () => {
       const { AddNewBoardDialog } = await import("../components/add_new_board_dialog.js");
       const dialog = AddNewBoardDialog.init();
       document.querySelector("body")?.appendChild(dialog);
@@ -48,8 +50,26 @@ export class BoardsList {
       dialog.showModal();
     });
 
+    // btn "Edit Board"
+    btns[1].addEventListener("click", async () => {
+      const { EditBoardDialog } = await import("../components/edit_board_dialog.js");
+      const dialog = EditBoardDialog.init();
+      document.querySelector("body")?.appendChild(dialog);
+      // @ts-ignore
+      dialog.showModal();
+    });
+
+    // btn "Delete Board"
+    btns[2].addEventListener("click", async () => {
+      const { DeleteBoardDialog } = await import("../components/delete_board_dialog.js");
+      const dialog = DeleteBoardDialog.init();
+      document.querySelector("body")?.appendChild(dialog);
+      // @ts-ignore
+      dialog.showModal();
+    });
+
     // btn "Add New Task"
-    component.querySelector(".add-new-task-btn")?.addEventListener("click", async () => {
+    btns[3].addEventListener("click", async () => {
       const { AddNewTaskDialog } = await import("../components/add_new_task_dialog.js");
       const dialog = AddNewTaskDialog.init();
       document.querySelector("body")?.appendChild(dialog);
@@ -67,22 +87,23 @@ export class BoardsList {
       ul.querySelector("li.selected")?.classList.remove("selected");
       // @ts-ignore
       const data = event.detail;
-      ul.appendChild(	
+      ul.appendChild(
 	BoardsListItem.init({
-	  id:       data.id,
-	  title:    data.name,
-	  selected: true,
-	  locked:   Role.getRole() === roles.ANONYMOUS
+	  id: data.id,
+	  title: data.name,
+	  selected: true,	  
+	  locked: globalThis.role === "anonymous"
 	})
       );
 
       console.log("board:created");
     });
 
-    component.addEventListener("board:updated", (event) => {
+    component.addEventListener("board:updated", async (event) => {
       // @ts-ignore
       const data = event.detail;
-      insert(	
+      const { insert } = await import("./_helpers.js");
+      insert(
 	BoardsListItem.init({ id: data.id, title: data.name, selected: true }),
         "ul > li.selected",
         component
