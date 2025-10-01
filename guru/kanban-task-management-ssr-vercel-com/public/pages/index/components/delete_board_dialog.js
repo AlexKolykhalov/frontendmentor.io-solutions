509b75a4377292e95db6283834928960a19040bb
AlexKolykhalov <ak8647@rambler.ru>
2025-10-01 15:28:42 +0300

    guru:kanban-ssr-vercel Optimazed the component
    1. Using globalThis instead of global value
    2. Using a popup window instead of throwing an Error to the console

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/delete_board_dialog.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/delete_board_dialog.js
@@ -3,7 +3,6 @@
 import { BoardsList }               from "./boards_list.js";
 import { BoardsListItem }           from "./boards_list_item.js";
 import { MainHeader }               from "./main_header.js";
-import { Role, roles }              from "../../_shared/roles.js";
 import { emit, openRedirectDialog } from "./_helpers.js";
 
 export class DeleteBoardDialog {
@@ -19,7 +18,7 @@ export class DeleteBoardDialog {
                 </div>
                 <p class="fs-300 clr-n-600">Are you sure you want to delete the <strong class="clr-n-900-000">"${title?.textContent}"</strong> board? This action will remove all columns and tasks and cannot be reversed.</p>
                 <div class="row gap-l main-axis-end">
-                  <button class="fw-bold fs-300 pad-h-m clr-n-000 pad-v-sm border-radius-l bg-p-red">Delete</button>
+                  <button class="[ relative ] fw-bold fs-300 pad-h-l clr-n-000 pad-v-sm border-radius-l bg-p-red">Delete</button>
                 </div>
               </div>
             </dialog>`;
@@ -52,17 +51,30 @@ export class DeleteBoardDialog {
   static #handleEvents(component) {
     // delete btn
     component.querySelectorAll("button")[1].addEventListener("click", async function() {
+      const mainHeader = document.querySelector(`#${MainHeader.prefix}`);
       const boardsList = document.querySelector(`#${BoardsList.prefix}`);
-      if (!boardsList) throw new Error(`Can't find <article id="${BoardsList.prefix}">`);
+      if (!mainHeader) throw new Error(`Missing <article id="${MainHeader.prefix}">`);
+      if (!boardsList) throw new Error(`Missing <article id="${BoardsList.prefix}">`);
       const selectedBoardsListItem = document.querySelector(`#${BoardsList.prefix} ul > li.selected`);
       if (!selectedBoardsListItem) throw new Error(`Missing selected boards list item`);
       const selectedItemIdAttribute = selectedBoardsListItem.getAttribute("id");
       if (!selectedItemIdAttribute) throw new Error(`Missing ID attribute of the selected boards list item`);
       const boardID = selectedItemIdAttribute.slice(`${BoardsListItem.prefix}-`.length);
 
-      if (Role.getRole() === roles.ANONYMOUS) {
-	emit("board:deleted", boardID, boardsList); component.remove(); return;
+      if (globalThis.role === "anonymous") {
+	emit("board:deleted", boardID, boardsList);
+	emit("board:deleted", {}, mainHeader);
+	component.remove();
+	return;
       }
+
+      this.setAttribute("disabled", "");
+      // add indicator
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+      const loader = LoaderRipple.init();
+      loader.classList.add("clr-n-000");
+      loader.setAttribute("style", "--size: 25px; right: 5%;");
+      this.appendChild(loader);
 
       const url     = "http://localhost:4000/rpc/delete_board";
       const options = {
@@ -84,17 +96,43 @@ export class DeleteBoardDialog {
 	  return;
 	}
 
-	if (resAuthz.status !== 201) throw new Error("Get generate_authz_token error");
+	if (resAuthz.status !== 201) {
+	  const { PopUp } = await import("../../_shared/components/pop_up.js");
+	  document.body.appendChild(
+	    PopUp.init({
+	      title: "Authentication token error",
+	      message: "Something went wrong. Try again."
+	    })
+	  );
+	  this.removeAttribute("disabled");
+	  loader.remove();
+
+	  return;
+	}
 
 	localStorage.setItem("bearer", await resAuthz.json());
 	options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
 	response = await fetch(url, options);
       }
 
-      if (response.status !== 204) throw new Error("Unexpected response status");
+      if (response.status !== 204) {
+	const { PopUp } = await import("../../_shared/components/pop_up.js");
+	document.body.appendChild(
+	  PopUp.init({
+	    title: "Server error",
+	    message: "Something went wrong. Try again."
+	  })
+	);
+	this.removeAttribute("disabled");
+	loader.remove();
+
+	return;
+      }
 
       // remove deleted element from the "Boards list"
       emit("board:deleted", boardID, boardsList);
+      // update deleteBoardBtn disable status
+      emit("board:deleted", {}, mainHeader);
       component.remove();
     });
 
