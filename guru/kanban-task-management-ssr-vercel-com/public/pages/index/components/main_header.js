4bf3a7dc4414a65bec5e3e56f391a3aaf8ba5b8f
AlexKolykhalov <ak8647@rambler.ru>
2025-10-01 16:01:34 +0300

    guru:kanban-ssr-vercel Optimazed the component
    1. Added "signout", "delete user" buttons
    2. Added fn updateBoardBtnDisabledStatus
    3. Added fn signout

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/main_header.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/main_header.js
@@ -1,10 +1,13 @@
 // @ts-check
 
-import { Board } from "./board.js";
+import { Board }              from "./board.js";
+import { BoardsList }         from "./boards_list.js";
+import { openRedirectDialog } from "./_helpers.js";
 
+// listens to [board:selected, created, updated, deleted]
 export class MainHeader {
-  
-  static prefix   = "main_header";  // using in add_new_board_dialog.js
+
+  static prefix   = "main_header"; // using in add_new_board_dialog.js
   static selector = `#${this.prefix}`;
 
   /**
@@ -13,12 +16,18 @@ export class MainHeader {
    * @returns {string} HTML string
    */
   static template(board) {
+    const deleteBoardBtn = globalThis.boardsList.length === 1 ?
+	  `<button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Delete board" aria-label="Delete board"><img src="images/svg/icon-delete-gray.svg" alt=""></button>` :
+	  `<button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Delete board" aria-label="Delete board"><img src="images/svg/icon-delete.svg" alt=""></button>`;
+    const deleteUserBtn = globalThis.role === "auth" ?
+	  `<button class="transparent pad-sm border-radius-sm" title="Delete user" aria-label="Delete user"><img src="images/svg/icon-user-delete.svg" alt=""></button>` :
+	  "";
     const path = `data-path="http://localhost:3000/pages/index/components/main_header.js"`;
 
     return `<header id="${this.prefix}" class="bg-n-000-800" style="height: 4.5rem;" ${path}>
               <div class="with-left-sidebar">
               	<div class="[ m:display-none ] pad-h-l pad-v-m">
-                  <img src="/images/svg/logo-light.svg" height=30 alt="Logo">
+                  <img src="/images/svg/logo-light.svg" height=30 alt="Logo desktop view">
                 </div>
               	<div class="row gap-m main-axis-space-between cross-axis-center pad-m">
               	  <div class="row gap-d-sm-m cross-axis-center">
@@ -30,10 +39,14 @@ export class MainHeader {
                             aria-label="Dropdown list toggle button">
                       <img src="images/svg/icon-chevron-down.svg" alt="Icon chevron down">
                     </button>
-                    <button class="transparent pad-sm border-radius-sm" aria-label="Edit board"><img src="images/svg/icon-edit.svg" alt=""></button>
-                    <button class="transparent pad-sm border-radius-sm" aria-label="Delete board"><img src="images/svg/icon-delete.svg" alt=""></button>
+                    <button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Edit board" aria-label="Edit board"><img src="images/svg/icon-edit.svg" alt=""></button>
+                    ${deleteBoardBtn}
               	  </div>
-              	  <button class="[ m:display-none ] fw-bold fs-500 clr-n-000 bg-p-purple pad-h-m pad-v-sm border-radius-l">+ Add New Task</button>
+                  <div class="row gap-d-sm-m cross-axis-center">
+              	    <button class="[ m:display-none ] fw-bold fs-d-300-500 clr-n-000 bg-p-purple pad-h-m pad-v-sm border-radius-l">+ Add New Task</button>
+                    ${deleteUserBtn}
+                    <button class="transparent pad-sm border-radius-sm" title="Sign out" aria-label="Sign out"><img src="images/svg/icon-exit.svg" alt=""></button>
+                  </div>
               	</div>
               </div>
             </header>`;
@@ -95,6 +108,25 @@ export class MainHeader {
       dialog.showModal();
     });
 
+    // btn "Signout" in "anon" mode or "Delete user" in "auth" mode
+    component.querySelectorAll("button")[4].addEventListener("click", async () => {
+      if (globalThis.role === "auth") {
+	const { DeleteUserDialog } = await import("../components/delete_user_dialog.js");
+	const dialog = DeleteUserDialog.init();
+	document.querySelector("body")?.appendChild(dialog);
+	// @ts-ignore
+	dialog.showModal();
+      }
+      if (globalThis.role === "anonymous") await signout();
+    });
+
+    // btn "Signout" in "auth" mode
+    component.querySelectorAll("button")[5]?.addEventListener("click", async () => {
+      await signout();
+    });
+
+    // ***ADDITIONAL LISTENERS***
+
     component.addEventListener("board:selected", (event) => {
       // @ts-ignore
       updateName(event.detail.name);
@@ -105,6 +137,7 @@ export class MainHeader {
     component.addEventListener("board:created", (event) => {
       // @ts-ignore
       updateName(event.detail.name);
+      updateBoardBtnDisabledStatus();
 
       console.log("board:created");
     });
@@ -116,6 +149,14 @@ export class MainHeader {
       console.log("board:updated");
     });
 
+    component.addEventListener("board:deleted", () => {
+      updateBoardBtnDisabledStatus();
+
+      console.log("board:deleted");
+    });
+
+    // ***ADDITIONAL FUNCTIONS***
+
     /**
      * @param {string} name
      *
@@ -126,6 +167,88 @@ export class MainHeader {
       if (!h2) throw new Error("Missing <h2>");
 
       h2.textContent = name;
+    }
+
+    function updateBoardBtnDisabledStatus() {
+      const deleteBoardBtn     = component.querySelectorAll("button")[2];
+      const headerOfBoardsList = document.querySelector(`#${BoardsList.prefix} > header`);
+      if (!deleteBoardBtn)     throw new Error(`Missing "deleteBoardBtn"`);
+      if (!headerOfBoardsList) throw new Error(`Missing #${BoardsList.prefix} > header`);
+
+      const textContent = headerOfBoardsList.textContent;
+      const btnImage    = deleteBoardBtn.querySelector("img");
+      if (!textContent) throw new Error(`Missing content of #${BoardsList.prefix} > header`);
+      if (!btnImage)    throw new Error(`Missing deleteBoardBtn > img`);
+
+      const countOfBoards = Number(textContent.slice(textContent.indexOf("(") + 1, textContent.indexOf(")")));
+      if (countOfBoards > 1) {
+	deleteBoardBtn.removeAttribute("disabled");
+	btnImage.src = "images/svg/icon-delete.svg";
+      } else {
+	deleteBoardBtn.setAttribute("disabled", "");
+	btnImage.src = "images/svg/icon-delete-gray.svg";
+      }
+    }
+
+    /**
+     * Implements procedure of signout.
+     *
+     * @throws {Error} Throws an error
+     * if authz_token could not be generating or response.status !== 204
+     * @returns {Promise<void>}
+     */
+    async function signout() {
+      const url     = "http://localhost:3000/api/signout";
+      const options = {
+	method: "POST",
+	headers: {
+	  "Content-Type": "application/json",
+	  "Authorization": `Bearer ${ localStorage.getItem("bearer") }`,
+	},
+      };
+
+      // [Errors 400, 401, 403, 500] [Success 204]
+      let response = await fetch(url, options);
+
+      if (response.status === 401) {
+	const resAuthz = await fetch("http://localhost:3000/api/generate_authz_token", { method: "POST" });
+	if (resAuthz.status === 401) {
+	  await openRedirectDialog();
+
+	  return;
+	}
+
+	if (resAuthz.status !== 201) {
+	  const { PopUp } = await import("../../_shared/components/pop_up.js");
+	  document.body.appendChild(
+	    PopUp.init({
+	      title: "Authentication token error",
+	      message: "Something went wrong. Try again."
+	    })
+	  );
+
+	  return;
+	}
+
+	localStorage.setItem("bearer", await resAuthz.json());
+	options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
+	response = await fetch(url, options);
+      }
+
+      if (response.status !== 204) {
+	const { PopUp } = await import("../../_shared/components/pop_up.js");
+	document.body.appendChild(
+	  PopUp.init({
+	    title: "Server error",
+	    message: "Something went wrong. Try again."
+	  })
+	);
+
+	return;
+      }
+
+      localStorage.removeItem("bearer");
+      window.location.replace("/auth");
     }
   }
 }
