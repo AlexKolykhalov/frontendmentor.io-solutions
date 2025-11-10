3f70ac390b719a48138e038d96fc10937fef7d81
AlexKolykhalov <ak8647@rambler.ru>
2025-11-10 11:24:31 +0300

    guru:kanban-ssr-vercel Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.
    3. Use a new api paths (*/v1/boards, */v1/tasks, etc).

--- guru/kanban-task-management-ssr-vercel-com/public/pages/auth/components/auth_form.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/auth/components/auth_form.js
@@ -1,19 +1,17 @@
 // @ts-check
 
 export class AuthForm {
-
-  static prefix   = "auth_form";
-  static selector = `#${this.prefix}`;
+  static prefix = "auth-form";
 
   /** @returns {string} HTML string */
   static template() {
-    const path = `data-path="http://localhost:3000/pages/auth/components/auth_form.js"`;
+    globalThis.paths[this.prefix] = "/pages/auth/components/auth_form.js";
 
-    return `<div id="${this.prefix}" class="column gap-l" ${path}>
+    return `<div class="column gap-l" data-prefix="${this.prefix}">
 	      <div class="column gap-m">
 	        <div class="column gap-sm">
-	          <label class="clr-n-600-000" for="login">Login</label>
-	          <input class="pad-sm clr-n-900-000 bg-n-100-900" id="login" maxlength="50">
+	          <label class="clr-n-600-000" for="email">Email</label>
+	          <input class="pad-sm clr-n-900-000 bg-n-100-900" id="email" type="email">
 	        </div>
 	        <div class="column gap-sm">
 	          <label class="clr-n-600-000" for="password">Password (more than 5 symbols)</label>
@@ -30,25 +28,23 @@ export class AuthForm {
 
   /**
    * @param {Element} component
-   *
    * @returns {void}
    */
   static handleEvents(component) {
     const contrsBtn = component.querySelectorAll("button");
 
     /** @type {HTMLInputElement|null} */
-    const inputLogin = component.querySelector("#login");
-    if (!inputLogin) throw new Error(`Missing <input id="login">`);
-    inputLogin.addEventListener("input", function () { this.removeAttribute("style") } );
+    const inputEmail = component.querySelector("#email");
+    if (!inputEmail) throw new Error(`<input id="email"> is missing`);
+    inputEmail.addEventListener("input", function () { this.removeAttribute("style") } );
 
     /** @type {HTMLInputElement|null} */
     const inputPassword = component.querySelector("#password");
-    if (!inputPassword) throw new Error(`Missing <input id="password">`);
+    if (!inputPassword) throw new Error(`<input id="password"> is missing`);
     inputPassword.addEventListener("input", function () { this.removeAttribute("style") } );
 
     // sign in (anonymously)
     contrsBtn[0].addEventListener("click", async function() {
-      this.setAttribute("disabled", "");
       // add indicator
       const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
       const loader = LoaderRipple.init();
@@ -56,34 +52,28 @@ export class AuthForm {
       loader.setAttribute("style", "--size: 25px; right: 5%;");
       this.appendChild(loader);
 
-      // [Errors 400, 401, 500] [Success 200]
-      const response = await fetch(`http://localhost:3000/api/signin`, { method: "POST" });
+      // disable SignIn btn
+      this.setAttribute("disabled", "");
+
+      // [Errors 400, 500] [Success 200]
+      const response = await fetch(`http://localhost:3000/v1/signin`, { method: "POST" });
+
       if (response.status !== 200) {
 	this.removeAttribute("disabled");
 	loader.remove();
-
-	const { PopUp } = await import("../../_shared/components/pop_up.js");
-	document.body.appendChild(
-	  PopUp.init({
-	    title: response.status === 401 ? "Unauthorized" : "Server error",
-	    message: response.status === 401 ?
-	      "Invalid credentials." :
-	      "Something went wrong. Try again."
-	  })
-	);
+	const { openPopUp } = await import("../../_shared/functions.js");
+	await openPopUp("Server error", "Something went wrong. Try again.");
 
 	return;
       }
-      localStorage.setItem("bearer", await response.json());
+
       window.location.replace("/"); // redirect to index.html
     });
 
     // sign in (login & password)
     contrsBtn[1].addEventListener("click", async function() {
-
       if (!validation()) return;
 
-      this.setAttribute("disabled", "");
       // add indicator
       const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
       const loader = LoaderRipple.init();
@@ -91,14 +81,17 @@ export class AuthForm {
       loader.setAttribute("style", "--size: 25px; right: 5%;");
       this.appendChild(loader);
 
-      // [Errors 400, 401, 500] [Success 200]
+      // disable SignIn btn
+      this.setAttribute("disabled", "");
+
+      // [Errors 400, 500] [Success 200]
       const response = await fetch(
-	`http://localhost:3000/api/signin`,
+	`http://localhost:3000/v1/signin`,
 	{
 	  method: "POST",
 	  headers: { "Content-Type": "application/json" },
 	  body: JSON.stringify({
-	    login:    inputLogin.value.trim(),
+	    email:    inputEmail.value.trim(),
 	    password: inputPassword.value.trim(),
 	  })
 	}
@@ -107,30 +100,23 @@ export class AuthForm {
       if (response.status !== 200) {
 	this.removeAttribute("disabled");
 	loader.remove();
-
-	const { PopUp } = await import("../../_shared/components/pop_up.js");
-	document.body.appendChild(
-	  PopUp.init({
-	    title: response.status === 401 ? "Unauthorized" : "Server error",
-	    message: response.status === 401 ?
-	      "Invalid credentials." :
-	      "Something went wrong. Try again."
-	  })
+	const { openPopUp } = await import("../../_shared/functions.js");
+	await openPopUp(
+	  response.status === 500 ? "Server error" : "Signin error",
+	  await response.json()
 	);
 
 	return;
       }
 
-      localStorage.setItem("bearer", await response.json());
       window.location.replace("/"); // redirect to index.html
     });
 
     // sign up
     contrsBtn[2].addEventListener("click", async function() {
-      
+
       if (!validation()) return;
 
-      this.setAttribute("disabled", "");
       // add indicator
       const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
       const loader = LoaderRipple.init();
@@ -138,14 +124,17 @@ export class AuthForm {
       loader.setAttribute("style", "--size: 25px; right: 5%;");
       this.appendChild(loader);
 
-      // [Errors 400, 401, 500] [Success 201]
+      // disable SignIn btn
+      this.setAttribute("disabled", "");
+
+      // [Errors 400, 401, 422, 500] [Success 201]
       const response = await fetch(
-	`http://localhost:3000/api/signup`,
+	`http://localhost:3000/v1/signup`,
 	{
 	  method: "POST",
 	  headers: { "Content-Type": "application/json" },
 	  body: JSON.stringify({
-	    login:    inputLogin.value.trim(),
+	    email:    inputEmail.value.trim(),
 	    password: inputPassword.value.trim(),
 	  })
 	}
@@ -154,39 +143,33 @@ export class AuthForm {
       if (response.status !== 201) {
 	this.removeAttribute("disabled");
 	loader.remove();
-
-	const { PopUp } = await import("../../_shared/components/pop_up.js");
-	document.body.appendChild(
-	  PopUp.init({
-	    title: response.status === 409 ? "Conflict" : "Server error",
-	    message: response.status === 409 ?
-	      "Login already exists." :
-	      "Something went wrong. Try again."
-	  })
+	const { openPopUp } = await import("../../_shared/functions.js");
+	await openPopUp(
+	  response.status === 500 ? "Server error" : "Signup error",
+	  response.status === 500 ? "Something went wrong. Try again." : await response.json()
 	);
 
 	return;
       }
 
-      localStorage.setItem("bearer", await response.json());      
       window.location.replace("/"); // redirect to index.html
     });
 
-    // helper functions
+    // *** ADDITIONAL FUNCTIONS ***
 
     /** @returns {boolean} */
     function validation() {
       /** @type {HTMLInputElement|null} */
-      const inputLogin    = component.querySelector("#login");
+      const inputEmail    = component.querySelector("#email");
       /** @type {HTMLInputElement|null} */
       const inputPassword = component.querySelector("#password");
-      if (!inputLogin)    throw new Error(`Missing <input id="login">`);
-      if (!inputPassword) throw new Error(`Missing <input id="password">`);
+      if (!inputEmail)    throw new Error(`<input id="email"> is missing`);
+      if (!inputPassword) throw new Error(`<input id="password"> is missing`);
 
       let isValid = true;
 
-      if (!inputLogin.value.trim()) {
-	inputLogin.setAttribute("style", "border-color: red;");
+      if (!inputEmail.value.trim()) {
+	inputEmail.setAttribute("style", "border-color: red;");
 	isValid = false;
       }
 
