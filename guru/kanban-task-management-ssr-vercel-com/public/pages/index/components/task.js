f5886bb48edbaad283ac3083239a1e3507c8e568
AlexKolykhalov <ak8647@rambler.ru>
2025-11-10 11:18:06 +0300

    guru:kanban-ssr-vercel Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.
    3. Use a new api paths (*/v1/boards, */v1/tasks, etc).
    4. Removed all code that uses localStorage and token reauthentication operations.

--- guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/task.js
+++ guru/kanban-task-management-ssr-vercel-com/public/pages/index/components/task.js
@@ -1,14 +1,14 @@
 // @ts-check
 
 /**
- * @typedef {Object} SubtaskType
+ * @typedef  {Object}  SubtaskType
  * @property {string}  id
  * @property {string}  title
  * @property {boolean} isCompleted
  */
 
 /**
- * @typedef {Object} TaskType
+ * @typedef  {Object}        TaskType
  * @property {string}        id
  * @property {string}        title
  * @property {string}        description
@@ -16,32 +16,26 @@
  */
 
 /**
- * @typedef {Object} TaskComponentType
+ * @typedef  {Object}   TaskComponentType
  * @property {TaskType} task
  * @property {boolean}  [locked] By default "false".
  */
 
 export class Task {
-
-  static prefix   = "task"; // using in column.js
-  static selector = `[id^=${this.prefix}-]`;  
+  static prefix = "task"; // using in column.js
 
   /**
    * @param {TaskComponentType} props
-   *
    * @returns {string} HTML string
    */
   static template(props) {
-
-    const isSSR = typeof window === "undefined" && typeof document === "undefined";
-    const classes    = props.locked ? "[ relative ] task-item" : "task-item";
-    const lockedAttr = props.locked ? `data-locked="true"`: "";
-    const lockedImg  = props.locked ?
+    const lockedImg = props.locked ?
 	  `<img class="absolute" src="images/svg/icon-locked.svg" alt="" width="16" height="16" style="top: 7px; right: 7px;">`:
 	  "";
-    const path = isSSR ? `data-path="http://localhost:3000/pages/index/components/task.js"` : "";
-    
-    return `<li id="${this.prefix}-${props.task.id}" class="${classes}" draggable="true" ${lockedAttr} ${path}}>
+    if (typeof window === "undefined" && typeof document === "undefined")
+      globalThis.paths[this.prefix] = "/pages/index/components/task.js";
+
+    return `<li class="[ relative ] task-item" data-prefix="${this.prefix}" data-id="${props.task.id}" ${props.locked ? "data-locked": ""} draggable="true">
               <p class="fw-bold">${props.task.title}</p>
               <p class="fs-200 fw-bold clr-n-600">${props.task.subtasks.filter(item => item.isCompleted).length} of ${props.task.subtasks.length} Subtasks</p>
               ${lockedImg}
@@ -50,23 +44,21 @@ export class Task {
 
   /**
    * @param {TaskComponentType} props
-   *
    * @returns {Element}
    */
   static init(props) {
     return this.#create(props);
-  }  
+  }
 
   /**
    * @param {TaskComponentType} props
-   *
    * @returns {Element}
    */
   static #create(props) {
     const template     = document.createElement("template");
     template.innerHTML = this.template(props);
     const component    = template.content.firstElementChild;
-    if (!component)    throw new Error("Can't create \"Task\" component");
+    if (!component)    throw new Error(`Can't create ${this.name} component`);
 
     this.handleEvents(component);
 
@@ -79,74 +71,74 @@ export class Task {
    * @returns {void}
    */
   static handleEvents(component) {
-    // @ts-ignore
-    const isLocked = component.dataset.locked === "true"; delete component.dataset.locked;
-    
-    if (isLocked) component.addEventListener("click", async () => {
-      const { openAuthzDialog } = await import("./_helpers.js");
-      openAuthzDialog();
-    });
-    
-    if (!isLocked) {
+    // @ts-ignore    
+    if (component.hasAttribute("data-locked")) {
       component.addEventListener("click", async () => {
-	const id      = component.getAttribute("id")?.slice(`${Task.prefix}-`.length);
-	const url     = `http://localhost:4000/rpc/get_task?p_task_id=${id}`;
-	const options = {
-	  headers: {
-	    "Accept":         "application/vnd.pgrst.object+json",
-	    "Accept-Profile": "api",
-	    "Authorization":  `Bearer ${ localStorage.getItem("bearer") }`,
-	  }
-	};
-	// [Errors 401, 403, 404] [Success 200]
-	let response = await fetch(url, options);
+	const { openAuthzDialog } = await import("../functions.js");
+	openAuthzDialog();
+      })
 
-	if (response.status === 401) {
-	  // [Errors 400, 401, 500] [Success 201]
-	  const resAuthz = await fetch("http://localhost:3000/api/generate_authz_token", { method: "POST" });
-	  if (resAuthz.status === 401) {
-	    const { openRedirectDialog } = await import("./_helpers.js");
-	    openRedirectDialog();
+      component.removeAttribute("data-locked");
+    } else {
+      component.addEventListener("click", async () => {
+	// disable clicks on tasks or boards list items
+	if (document.querySelector(`.lds-ripple`)) return;
 
-	    return;
-	  }
+	// add indicator
+	const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+	const loader = LoaderRipple.init();
+	loader.setAttribute("style", "top: 5%; right: 2.5%"); // customize position inside task component
+	component.appendChild(loader);
 
-	  if (resAuthz.status !== 201) {
-	    const { PopUp } = await import("../../_shared/components/pop_up.js");
-	    document.body.appendChild(
-	      PopUp.init({
-		title:   "Authentication token error",
-		message: "Something went wrong. Try again."
-	      })
-	    );	    
-
-	    return;
-	  }
-
-	  localStorage.setItem("bearer", await resAuthz.json());
-	  options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
-	  response = await fetch(url, options);
-	};
+	const taskID = component.getAttribute("data-id");
+	if (!taskID) throw new Error("[data-id] is missing")
 	
-	if (response.status !== 200) {
-	  const { PopUp } = await import("../../_shared/components/pop_up.js");
-	  document.body.appendChild(
-	    PopUp.init({ title: "Server error", message: "Something went wrong. Try again." })
-	  );	  
+	// [Errors 401, 403, 404, 405, 500] [Success 200]
+	const response = await fetch(`http://localhost:3000/v1/tasks/${taskID}`);
+
+	if (response.status === 401 || response.status === 403) {
+	  if (response.status === 401) {
+	    const { openAuthzDialog } = await import("../functions.js");
+	    await openAuthzDialog();
+	  }
+
+	  if (response.status === 403) {
+	    const { openSessionExpiredDialog } = await import("../functions.js");
+	    await openSessionExpiredDialog();
+	  }
+	  
+	  loader.remove();
 
 	  return;
-	}
+	};
+
+	if (response.status === 404 || response.status !== 200) {
+	  const { openPopUp } = await import("../../_shared/functions.js");
+	  await openPopUp(
+	    response.status === 404 ? "Search error" : "Server error",
+	    response.status === 404 ? "Task not found" : "Something went wrong. Try again"
+	  );
+	  
+	  loader.remove();
+
+	  return;
+	};
 
 	const { TaskDialog } = await import("../components/task_dialog.js");
 	const dialog = TaskDialog.init(await response.json());
 	document.querySelector("body")?.appendChild(dialog);
 	// @ts-ignore
 	dialog.showModal();
+
+	loader.remove();
       });  
     }
+    
+    component.addEventListener("dragstart", (event) => { // dragover & drop see in column.js
+      if (!event.target) throw new Error("Target element is missing");
 
-    // @ts-ignore // dragover & drop see in column.js
-    component.addEventListener("dragstart", (e) => e.target?.classList.add("moving"));
+      // @ts-ignore
+      event.target.classList.add("moving");
+    }); 
   }
 }
-
