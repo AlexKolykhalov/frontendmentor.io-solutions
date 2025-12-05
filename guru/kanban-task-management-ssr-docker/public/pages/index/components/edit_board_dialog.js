14912af5be64c87b0814fd70cf7fc8bb46132fff
AlexKolykhalov <ak8647@rambler.ru>
2025-12-05 15:33:22 +0300

    guru:kanban-ssr-docker Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Using the standart replaceWith function instead of the custom "insert" function.
    3. Changed errors text.
    4. Use global variable, e.g "client_variables.is_anonymous".
    5. Use CustomEvent directly without the custom "emit" function.
    6. Use a new api paths (*/v1/boards, */v1/tasks, etc).

--- guru/kanban-task-management-ssr-docker/public/pages/index/components/edit_board_dialog.js
+++ guru/kanban-task-management-ssr-docker/public/pages/index/components/edit_board_dialog.js
@@ -1,20 +1,18 @@
 // @ts-check
 
-import { Board }                            from "./board.js";
-import { Column }                           from "./column.js";
-import { BoardsList }                       from "./boards_list.js";
-import { BoardsListItem }                   from "./boards_list_item.js";
-import { DynamicList }                      from "./dynamic_list.js";
-import { MainHeader }                       from "./main_header.js";
-import { emit, insert, openRedirectDialog } from "./_helpers.js";
+import { Board }       from "./board.js";
+import { Column }      from "./column.js";
+import { BoardsList }  from "./boards_list.js";
+import { DynamicList } from "./dynamic_list.js";
+import { MainHeader }  from "./main_header.js";
 
-// listens to [dynamic-list-item:changed, added, removed]
-export class EditBoardDialog {
-
-  static prefix = "edit_board_dialog"; // using in dynamic_list.js
-
+export class EditBoardDialog { // listens to [dynamic-list-item:changed, added, removed]
   /** @type { import("./board.js").BoardType } */
-  static #state =  { id: "", name: "", columns: [{ id: "", name: "", tasks: [] }] };
+  static #state =  {
+    id: "",
+    name: "",
+    columns: [{ id: "", name: "", tasks: [] }]
+  };
 
   /** @returns { import("./board.js").BoardType } */
   static #getState() {
@@ -23,12 +21,12 @@ export class EditBoardDialog {
 
   /** @param { import("./board.js").BoardType } value */
   static #setState(value) {
-    return this.#state = value;
+    this.#state = value;
   }
 
   /** @returns {string} HTML string */
   static #template() {
-    return `<dialog id="${this.prefix}" class="bg-n-000-800">
+    return `<dialog class="bg-n-000-800">
               <div class="column gap-l">
                 <div class="row gap-m main-axis-space-between">
                   <h2 class="fs-900 clr-n-900-000">Edit board</h2>
@@ -55,43 +53,39 @@ export class EditBoardDialog {
     /** @type { import("./column.js").ColumnType[] } */
     const columns = [];
 
-    const ul = document.querySelector(`#${Board.prefix} ul`); // current columns
-    if (!ul) throw new Error(`Missing #${Board.prefix} <ul>`);
+    const ul = document.querySelector(`[data-prefix="${Board.prefix}"] ul`);
+    if (!ul) throw new Error(`[data-prefix="${Board.prefix}"] <ul> is missing`);
 
     [...ul.children].forEach(column => {
-      const idAttr = column.getAttribute("id");
-      if (!idAttr) throw new Error(`Missing column's ID attribute`);
-      const h3 = column.querySelector("h3");
-      if (!h3) throw new Error(`Missing <li id="${Column.prefix}-"> <h3>`);
-      const textContent = h3.textContent;
-      if (!textContent) throw new Error(`Empty textContent in <li id="${Column.prefix}-"> <h3>`);
+      const columnID = column.getAttribute("data-id");
+      const h3       = column.querySelector("h3");
+      if (!columnID) throw new Error(`${Column.prefix} [data-id] is missing`);
+      if (!h3)       throw new Error(`${Column.prefix} <h3> is missing`);
 
-      const id = idAttr.slice(`${Column.prefix}-`.length);
+      const textContent = h3.textContent;
+      if (!textContent) throw new Error(`${Column.prefix} <h3> is empty`);
+
       // position of " (", e.g Todo (3)
       const name = textContent.slice(0, textContent.lastIndexOf("(") - 1);
 
       dynamicListItems.push({
-	id:                id,
+	id:                columnID,
 	value:             name,
 	placeholder:       "e.g. TODO",
 	deleteBtnDisabled: [...ul.children].length === 1
       });
 
-      columns.push({ id: id, name: name, tasks: []});
+      columns.push({ id: columnID, name: name, tasks: [] });
     });
 
-    const h2 = document.querySelector(`#${MainHeader.prefix} h2`);
-    if (!h2) throw new Error(`Missing <h2>`);
-    const title = h2.textContent;
-    if (!title) throw new Error(`<h2> text content is empty`);
-    const selectedBoardsListItem = document.querySelector(`#${BoardsList.prefix} ul > li.selected`);
-    if (!selectedBoardsListItem) throw new Error(`Missing selected boards list item`);
-    const selectedItemIdAttribute = selectedBoardsListItem.getAttribute("id");
-    if (!selectedItemIdAttribute) throw new Error(`Missing ID attribute of the selected boards list item`);
+    const title   = document.querySelector (`[data-prefix="${MainHeader.prefix}"] h2`)?.textContent;
+    const boardID = document.querySelector(`[data-prefix="${BoardsList.prefix}"] ul > li.selected`)?.getAttribute("data-id");
+    if (!title)   throw new Error(`[data-prefix="${MainHeader.prefix}"] <h2> is empty`);
+    if (!boardID) throw new Error(`[data-prefix="${BoardsList.prefix}"] ul > li.selected [data-id] is missing`);
 
     /** @type { import("./board.js").BoardType } */
     const state = {
-      id:      selectedItemIdAttribute.slice(`${BoardsListItem.prefix}-`.length),
+      id:      boardID,
       name:    title,
       columns: columns
     };
@@ -99,17 +93,17 @@ export class EditBoardDialog {
     this.#setState(state);
 
     const component = this.#create();
+    const dynamicList = component.querySelector("dynamic-list");
+    if (!dynamicList) throw new Error("<dynamic-list> is missing");
 
-    insert(
+    dynamicList.replaceWith(
       DynamicList.init({
 	title: "Board Columns",
 	items: dynamicListItems,
 	btnText: "+ Add New Column",
 	min: 1,
 	max: 5
-      }),
-      "dynamic-list",
-      component
+      })
     );
 
     return component;
@@ -120,7 +114,7 @@ export class EditBoardDialog {
     const template     = document.createElement("template");
     template.innerHTML = this.#template();
     const component    = template.content.firstElementChild;
-    if (!component)    throw new Error("Can't create \"EditBoardDialog\" component");
+    if (!component)    throw new Error(`Can't create ${this.name} component`);
 
     this.#handleEvents(component);
 
@@ -129,7 +123,6 @@ export class EditBoardDialog {
 
   /**
    * @param {Element} component
-   *
    * @returns {void}
    */
   static #handleEvents(component) {
@@ -138,7 +131,7 @@ export class EditBoardDialog {
     const revertBtn   = controlBtns[2];
 
     const input = component.querySelector("#board_name");
-    if (!input) throw new Error("Can't find <input id=\"board_name\">");
+    if (!input) throw new Error(`<input id="board_name"> is missing`);
 
     input.addEventListener("input", function() {
       this.removeAttribute("style");
@@ -147,111 +140,125 @@ export class EditBoardDialog {
 
     // save btn
     saveBtn.addEventListener("click", async function() {
-
       if (!validation()) return;
-
-      const mainHeader = document.querySelector(`#${MainHeader.prefix}`);
-      const board      = document.querySelector(`#${Board.prefix}`);
-      const boardsList = document.querySelector(`#${BoardsList.prefix}`);
-      if (!mainHeader) throw new Error(`Missing <header id="${MainHeader.prefix}">`);
-      if (!board)      throw new Error(`Missing <section id="${Board.prefix}">`);
-      if (!boardsList) throw new Error(`Missing <article id="${BoardsList.prefix}">`);
 
       const sendingBoardData = getDifferences();
 
-      if (sendingBoardData) {
-	if (globalThis.role === "anonymous") {
-	  sendingBoardData.columns.forEach(
-	    column => { if (column.id === "") column.id = crypto.randomUUID() }
-	  );
+      if (!sendingBoardData) return;
 
-	  emit("board:updated", sendingBoardData, board);      // rerenders the board
-	  emit("board:updated", sendingBoardData, boardsList); // changes name of the board
-	  emit("board:updated", sendingBoardData, mainHeader); // changes name of the board
+      const mainHeader = document.querySelector(`[data-prefix="${MainHeader.prefix}"]`);
+      const board      = document.querySelector(`[data-prefix="${Board.prefix}"]`);
+      const boardsList = document.querySelector(`[data-prefix="${BoardsList.prefix}"]`);
+      if (!mainHeader) throw new Error(`[data-prefix="${MainHeader.prefix}"] is missing`);
+      if (!board)      throw new Error(`[data-prefix="${Board.prefix}"] is missing`);
+      if (!boardsList) throw new Error(`[data-prefix="${BoardsList.prefix}"] is missing`);
 
-	  component.remove();
+      if (globalThis.client_variables.is_anonymous) {
+	sendingBoardData.columns.forEach(
+	  column => { if (column.id === "") column.id = crypto.randomUUID() }
+	);
 
-	  return;
-	}
+	component.remove(); // close this dialog
+	// @ts-ignore (mob-view click dropdownListToggleBtn)
+	document.querySelector(".dropdown-list-toggle-btn")?.click();
 
-	this.setAttribute("disabled", "");
-	// add indicator
-	const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
-	const loader = LoaderRipple.init();
-	loader.classList.add("clr-n-000");
-	loader.setAttribute("style", "--size: 25px; right: 5%;");
-	this.appendChild(loader);
+	[board, boardsList, mainHeader].forEach(item => {
+	  item.dispatchEvent(new CustomEvent("board:updated", { detail: sendingBoardData }))
+	});
 
-	const url     = "http://localhost:4000/rpc/update_board";
-	const options = {
-	  method: "POST",
-	  headers: {
-	    "Content-Type": "application/json",
-	    "Authorization": `Bearer ${ localStorage.getItem("bearer") }`,
-	  },
-	  body: JSON.stringify({ p_board: sendingBoardData }),
-	};
-        // [Errors 400, 401, 403] [Success 200]
-	let response = await fetch(url, options);
-
-	if (response.status === 401) {
-	  // [Errors 400, 401, 500] [Success 201]
-	  const resAuthz = await fetch("http://localhost:3000/api/generate_authz_token", { method: "POST" });
-	  if (resAuthz.status === 401) {
-	    await openRedirectDialog();
-
-	    return;
-	  }
-
-	  if (resAuthz.status !== 201) {
-	    const { PopUp } = await import("../../_shared/components/pop_up.js");
-	    document.body.appendChild(
-	      PopUp.init({
-		title: "Authentication token error",
-		message: "Something went wrong. Try again."
-	      })
-	    );
-	    this.removeAttribute("disabled");
-	    loader.remove();
-
-	    return;
-	  }
-
-	  localStorage.setItem("bearer", await resAuthz.json());
-	  options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
-	  response = await fetch(url, options);
-	};
-
-	if (response.status !== 200) {
-	  const { PopUp } = await import("../../_shared/components/pop_up.js");
-	  document.body.appendChild(
-	    PopUp.init({
-	      title: "Server error",
-	      message: "Something went wrong. Try again."
-	    })
-	  );
-	  this.removeAttribute("disabled");
-	  loader.remove();
-
-	  return;
-	}
-
-	const receivingBoardData = await response.json();
-
-	emit("board:updated", receivingBoardData, board);      // rerenders the board
-	emit("board:updated", receivingBoardData, boardsList); // changes name of the board
-	emit("board:updated", receivingBoardData, mainHeader); // changes name of the board
-
-        component.remove();
+	return;
       }
+
+      this.setAttribute("disabled", ""); // disabled saveBtn
+
+      // add indicator
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+      const loader = LoaderRipple.init();
+      loader.classList.add("clr-n-000");
+      loader.setAttribute("style", "--size: 25px; right: 5%;");
+      this.appendChild(loader);
+
+      const url     = `/v1/boards/${sendingBoardData.id}`;
+      const options = {
+	method: "PUT",
+	headers: {
+	  "Content-Type": "application/json",
+	  "Authorization": `Bearer ${ localStorage.getItem("bearer") }`,
+	},
+	body: JSON.stringify({ board: sendingBoardData }),
+      };
+
+      // [Errors 401, 403, 404, 405, 500] [Success 200]
+      let response = await fetch(url, options);
+
+      if (response.status === 401) {
+	// [Errors 400, 401, 500] [Success 201]
+	const responseBearer = await fetch("/v1/bearers/generate", { method: "POST" });
+
+	if (responseBearer.status !== 201) {
+	  if (responseBearer.status === 401) {
+	    const { openSessionExpiredDialog } = await import("../functions.js");
+	    await openSessionExpiredDialog();
+	  } else {
+	    const { openPopUp } = await import("../../_shared/functions.js");
+	    await openPopUp("Authentication token error", "Something went wrong. Try again.");
+	  }
+
+	  component.remove(); // close this dialog
+	  // @ts-ignore (mob-view click dropdownListToggleBtn)
+	  document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+	  return;
+	}
+
+	const bearer = await responseBearer.json();
+
+	localStorage.setItem("bearer", bearer);
+	options.headers.Authorization = `Bearer ${ bearer }`;
+	response = await fetch(url, options);
+      };
+
+      if (response.status === 403) {
+	const { openAuthzDialog } = await import("../functions.js");
+	await openAuthzDialog();
+
+	component.remove(); // close this dialog
+	// @ts-ignore (mob-view click dropdownListToggleBtn)
+	document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+	return;
+      }
+
+      if (response.status === 404 || response.status !== 200) {
+	const { openPopUp } = await import("../../_shared/functions.js");
+	await openPopUp(
+	  response.status === 404 ? "Search error" : "Server error",
+	  response.status === 404 ? "Board not found" : "Something went wrong. Try again"
+	);
+
+	component.remove(); // close this dialog
+	// @ts-ignore (mob-view click dropdownListToggleBtn)
+	document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+	return;
+      };
+
+      component.remove(); // close this dialog
+      // @ts-ignore (mob-view click dropdownListToggleBtn)
+      document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+      const receivedBoardData = await response.json();
+      [board, boardsList, mainHeader].forEach(item => {
+	item.dispatchEvent(new CustomEvent("board:updated", { detail: receivedBoardData }))
+      });
     });
 
-    revertBtn.addEventListener("click", function() {
+    revertBtn.addEventListener("click", () => {
       const state = EditBoardDialog.#getState();
 
       /** @type {HTMLInputElement|null} */
-      const inputBoardName  = component.querySelector("#board_name");
-      if (!inputBoardName)  throw new Error("Can't find <input id=\"board_name\">");
+      const inputBoardName = component.querySelector("#board_name");
+      if (!inputBoardName) throw new Error(`<input id="board_name"> is missing`);
 
       inputBoardName.value = state.name;
       inputBoardName.removeAttribute("style");
@@ -261,21 +268,23 @@ export class EditBoardDialog {
 	return {
 	  id: column.id,
 	  value: column.name,
+	  placeholder: "e.g. TODO",
 	  deleteBtnDisabled: state.columns.length === 1 ? true : false
 	};
-      });      
+      });
 
-      // revert dynamic list (create a new one and insert the modified one insteed)
-      insert(
+      // revert dynamic list (create a new one and insert the modified list insteed)
+      const currentDynamicList = component.querySelector("[data-id]");
+      if (!currentDynamicList) throw new Error("DynamicList is missing");
+
+      currentDynamicList.replaceWith(
 	DynamicList.init({
 	  title: "Board Columns",
 	  items: items,
 	  btnText: "+ Add New Column",
 	  min: 1,
 	  max: 5
-	}),	
-	`[id^=${DynamicList.prefix}-]`,
-	component
+	})
       );
 
       saveBtn.setAttribute("disabled", "");
@@ -283,7 +292,7 @@ export class EditBoardDialog {
     });
 
     const closeDialogBtn = component.querySelector('button[aria-label="close"]');
-    if (!closeDialogBtn) throw new Error("Can't find <button aria-label=\"close\">");
+    if (!closeDialogBtn) throw new Error(`<button aria-label="close"> is missing`);
     closeDialogBtn.addEventListener("click", () => component.remove());
 
     // *** ADDITIONAL LISTENERS ***
@@ -305,40 +314,13 @@ export class EditBoardDialog {
       }
     }
 
-    /** @returns {boolean} */
-    function validation() {
-      const inputBoardName  = component.querySelector("#board_name");
-      const listOfColumns = component.querySelector("ul");
-      if (!inputBoardName) throw new Error("Can't find <input id=\"board_name\">");
-      if (!listOfColumns)  throw new Error("Can't find <ul>");
-
-      let isValid = true;
-
-      // @ts-ignore
-      if (!inputBoardName.value.trim()) { // input (must not be empty)
-	inputBoardName.setAttribute("style", "border-color: red;");
-	isValid = false;
-      }
-
-      [...listOfColumns.children].forEach((item) => {
-	if (!item.querySelector("input")?.value.trim()) { // board of columns (must not be empty)
-	  item.querySelector("input")?.setAttribute("style", "border-color: red;");
-	  isValid = false;
-	}
-      });
-
-      return isValid;
-    }
-
-    /***
-     * @returns {import("./board.js").BoardType|null} Returns differences or null (no differences)
-     */
+    /** @returns {import("./board.js").BoardType|null} Returns differences or null (no differences) */
     function getDifferences() {
       /** @type {HTMLInputElement|null} */
       const boardNameInput = component.querySelector("#board_name");
       const listOfColumns  = component.querySelector("ul");
-      if (!boardNameInput) throw new Error("Can't find <input id=\"board_name\">");
-      if (!listOfColumns)  throw new Error("Can't find <ul>");
+      if (!boardNameInput) throw new Error(`<input id="board_name"> is missing`);
+      if (!listOfColumns)  throw new Error(`<ul> is missing`);
 
       let   flag      = false;
       let   addings   = [];
@@ -353,12 +335,10 @@ export class EditBoardDialog {
 	let isDelete = true;
 	for (let j = 0; j < [...listOfColumns.children].length; j++) {
 	  const input = [...listOfColumns.children][j].querySelector("input");
-	  if (!input) throw new Error("Missing <input> in <li> element");
-	  const idAttr = input.getAttribute("id");
-	  if (!idAttr) throw new Error("Missing ID attribute in <input>");
+	  if (!input) throw new Error("<input> is missing");
 
-	  let id = idAttr.slice(`x-`.length);
-	  if (i === 0 && id.length !== 36) { // i === 0 it's helps push only once
+	  const id = input.getAttribute("data-id");
+	  if (i === 0 && !id) { // i === 0 it's helps push only once
 	    addings.push({ id: "", name: input.value.trim(), tasks: [] }); // ADD
 	    flag = true;
 	  };
@@ -380,6 +360,31 @@ export class EditBoardDialog {
       stateData.columns.push(...addings);
 
       return flag ? stateData : null;
+    }
+
+    /** @returns {boolean} */
+    function validation() {
+      const inputBoardName = component.querySelector("#board_name");
+      const listOfColumns  = component.querySelector("ul");
+      if (!inputBoardName) throw new Error(`<input id="board_name"> is missing`);
+      if (!listOfColumns)  throw new Error("<ul> is missing");
+
+      let isValid = true;
+
+      // @ts-ignore
+      if (!inputBoardName.value.trim()) { // input (must not be empty)
+	inputBoardName.setAttribute("style", "border-color: red;");
+	isValid = false;
+      }
+
+      [...listOfColumns.children].forEach((item) => {
+	if (!item.querySelector("input")?.value.trim()) { // Board columns (must not be empty)
+	  item.querySelector("input")?.setAttribute("style", "border-color: red;");
+	  isValid = false;
+	}
+      });
+
+      return isValid;
     }
   }
 }
