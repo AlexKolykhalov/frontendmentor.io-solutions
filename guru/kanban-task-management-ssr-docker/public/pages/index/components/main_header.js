0ad01c4384767cbde7e4dfd2ce6861ac841b2618
AlexKolykhalov <ak8647@rambler.ru>
2025-12-05 15:37:54 +0300

    guru:kanban-ssr-docker Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.
    3. Use global variable, e.g "client_variables.is_anonymous".
    4. Use a new api paths (*/v1/boards, */v1/tasks, etc).

--- guru/kanban-task-management-ssr-docker/public/pages/index/components/main_header.js
+++ guru/kanban-task-management-ssr-docker/public/pages/index/components/main_header.js
@@ -1,30 +1,25 @@
 // @ts-check
 
-import { Board }              from "./board.js";
-import { BoardsList }         from "./boards_list.js";
-import { openRedirectDialog } from "./_helpers.js";
+import { BoardsList } from "./boards_list.js";
 
-// listens to [board:selected, created, updated, deleted]
-export class MainHeader {
-
-  static prefix   = "main_header"; // using in add_new_board_dialog.js
-  static selector = `#${this.prefix}`;
+export class MainHeader { // listens to [board:selected, created, updated, deleted]
+  static prefix = "main-header"; // using in add_new_board_dialog.js
 
   /**
    * @param {import("./board.js").BoardType} board
-   *
    * @returns {string} HTML string
    */
   static template(board) {
-    const deleteBoardBtn = globalThis.boardsList.length === 1 ?
+    const deleteBoardBtn = globalThis.server_variables.boards_list_length === 1 ?
 	  `<button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Delete board" aria-label="Delete board" disabled><img src="images/svg/icon-delete-gray.svg" alt=""></button>` :
 	  `<button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Delete board" aria-label="Delete board"><img src="images/svg/icon-delete.svg" alt=""></button>`;
-    const deleteUserBtn = globalThis.role === "auth" ?
-	  `<button class="transparent pad-sm border-radius-sm" title="Delete user" aria-label="Delete user"><img src="images/svg/icon-user-delete.svg" alt=""></button>` :
-	  "";
-    const path = `data-path="http://localhost:3000/pages/index/components/main_header.js"`;
+    const deleteUserBtn = globalThis.server_variables.is_anonymous ?
+	  "" :
+	  `<button class="transparent pad-sm border-radius-sm" title="Delete user" aria-label="Delete user"><img src="images/svg/icon-user-delete.svg" alt=""></button>`;
 
-    return `<header id="${this.prefix}" class="bg-n-000-800" style="height: 4.5rem;" ${path}>
+    globalThis.paths[this.prefix] = "/pages/index/components/main_header.js";
+
+    return `<header class="bg-n-000-800" data-prefix="${this.prefix}" style="height: 4.5rem;">
               <div class="with-left-sidebar">
               	<div class="[ m:display-none ] pad-h-l pad-v-m">
                   <img src="/images/svg/logo-light.svg" height=30 alt="Logo desktop view">
@@ -37,7 +32,7 @@ export class MainHeader {
                             aria-controls="dropdown_list"
                             aria-expanded="false"
                             aria-label="Dropdown list toggle button">
-                      <img src="images/svg/icon-chevron-down.svg" alt="Icon chevron down">
+                      <img src="images/svg/icon-chevron-down.svg" width="15" alt="Icon chevron down">
                     </button>
                     <button class="[ m:display-none ] transparent pad-sm border-radius-sm" title="Edit board" aria-label="Edit board"><img src="images/svg/icon-edit.svg" alt=""></button>
                     ${deleteBoardBtn}
@@ -45,7 +40,7 @@ export class MainHeader {
                   <div class="row gap-d-sm-m cross-axis-center">
               	    <button class="[ m:display-none ] fw-bold fs-d-300-500 clr-n-000 bg-p-purple pad-h-m pad-v-sm border-radius-l">+ Add New Task</button>
                     ${deleteUserBtn}
-                    <button class="transparent pad-sm border-radius-sm" title="Sign out" aria-label="Sign out"><img src="images/svg/icon-exit.svg" alt=""></button>
+                    <button class="[ relative ] transparent pad-sm border-radius-sm" title="Sign out" aria-label="Sign out"><img src="images/svg/icon-exit.svg" alt=""></button>
                   </div>
               	</div>
               </div>
@@ -54,36 +49,34 @@ export class MainHeader {
 
   /**
    * @param {Element} component
-   *
    * @returns {void}
    */
   static handleEvents(component) {
     // Dropdown list toggle btn
     const dropdownListToggleBtn = document.querySelector(".dropdown-list-toggle-btn");
-    if (!dropdownListToggleBtn) throw new Error("Missing .dropdown-list-toggle-btn");
-    dropdownListToggleBtn.addEventListener("click", function() {
-      const dropdownList = document.querySelector(".dropdown-list");
-      if (!dropdownList) throw new Error(`Missing .dropdown-list`);
+    if (!dropdownListToggleBtn) throw new Error(".dropdown-list-toggle-btn is missing");
+    dropdownListToggleBtn.addEventListener("click", async function() {
+      const dropdownList = document.querySelector("#dropdown_list"); // see index.html
+      if (!dropdownList) throw new Error(`#dropdown_list is missing`);
 
       dropdownList.classList.toggle("m:display-none");
       if (this.getAttribute("aria-expanded") === "true") {
 	this.setAttribute("aria-expanded", "false");
-	// remove backdrop
-	document.querySelector(".backdrop")?.remove();
+	document.querySelector(`[data-prefix="backdrop"]`)?.remove(); // remove backdrop
       } else {
 	this.setAttribute("aria-expanded", "true");
-	// add backdrop
-	const backdrop = document.createElement("div");
-	backdrop.classList.add("backdrop");
+	const backdrop = document.createElement("div"); // create backdrop
 	backdrop.classList.add("md:display-none");
+	backdrop.setAttribute("data-prefix", "backdrop");
 	backdrop.setAttribute("style", "background: rgba(0,0,0,0.5); position: fixed; width: 100%; height: 100%");
-	document.querySelector(`#${Board.prefix}`)?.appendChild(backdrop);
+	const { Board } = await import("./board.js");
+	document.querySelector(`[data-prefix="${Board.prefix}"]`)?.appendChild(backdrop);
       }
     });
 
     // btn "Edit board"
     component.querySelectorAll("button")[1].addEventListener("click", async () => {
-      const { EditBoardDialog } = await import("../components/edit_board_dialog.js");
+      const { EditBoardDialog } = await import("./edit_board_dialog.js");
       const dialog = EditBoardDialog.init();
       document.querySelector("body")?.appendChild(dialog);
       // @ts-ignore
@@ -109,42 +102,74 @@ export class MainHeader {
     });
 
     // btn "Signout" in "anon" mode or "Delete user" in "auth" mode
-    component.querySelectorAll("button")[4].addEventListener("click", async () => {
-      if (globalThis.role === "auth") {
+    component.querySelectorAll("button")[4].addEventListener("click", async function() {
+      if (globalThis.client_variables.is_anonymous) {
+	const img = this.querySelector("img");
+	if (!img) throw new Error("Signout btn <img> is missing");
+
+	const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+	const loader = LoaderRipple.init();
+	loader.setAttribute("style", "--size: 20px; position: relative;");
+	this.replaceChildren(loader);
+
+	try {
+	  await signout();
+	} catch (error) {
+	  this.replaceChildren(img);
+	}
+      }
+      if (!globalThis.client_variables.is_anonymous) {
 	const { DeleteUserDialog } = await import("../components/delete_user_dialog.js");
 	const dialog = DeleteUserDialog.init();
 	document.querySelector("body")?.appendChild(dialog);
 	// @ts-ignore
 	dialog.showModal();
       }
-      if (globalThis.role === "anonymous") await signout();
     });
 
     // btn "Signout" in "auth" mode
-    component.querySelectorAll("button")[5]?.addEventListener("click", async () => {
-      await signout();
+    component.querySelectorAll("button")[5]?.addEventListener("click", async function() {
+      const img = this.querySelector("img");
+      if (!img) throw new Error("Signout btn <img> is missing");
+
+      const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
+      const loader = LoaderRipple.init();
+      loader.setAttribute("style", "--size: 20px; position: relative;");
+      this.replaceChildren(loader);
+
+      try {
+	await signout();
+      } catch (error) {
+	this.replaceChildren(img);
+      }
     });
 
     // ***ADDITIONAL LISTENERS***
 
     component.addEventListener("board:selected", (event) => {
+      /** @type {import("./board.js").BoardType} */
       // @ts-ignore
-      updateName(event.detail.name);
+      const board = event.detail;
+      updateName(board.name);
 
       console.log("board:selected");
     });
 
     component.addEventListener("board:created", (event) => {
+      /** @type {import("./board.js").BoardType} */
       // @ts-ignore
-      updateName(event.detail.name);
+      const board = event.detail;
+      updateName(board.name);
       updateBoardBtnDisabledStatus();
 
       console.log("board:created");
     });
 
     component.addEventListener("board:updated", (event) => {
+      /** @type {import("./board.js").BoardType} */
       // @ts-ignore
-      updateName(event.detail.name);
+      const board = event.detail;
+      updateName(board.name);
 
       console.log("board:updated");
     });
@@ -157,31 +182,24 @@ export class MainHeader {
 
     // ***ADDITIONAL FUNCTIONS***
 
-    /**
-     * @param {string} name
-     *
-     * @returns {void} HTML string
-     */
+    /** @param {string} name */
     function updateName(name) {
       const h2 = component.querySelector("h2");
-      if (!h2) throw new Error("Missing <h2>");
+      if (!h2) throw new Error(`<h2> is missing`);
 
       h2.textContent = name;
     }
 
     function updateBoardBtnDisabledStatus() {
-      const deleteBoardBtn     = component.querySelectorAll("button")[2];
-      const headerOfBoardsList = document.querySelector(`#${BoardsList.prefix} > header`);
-      if (!deleteBoardBtn)     throw new Error(`Missing "deleteBoardBtn"`);
-      if (!headerOfBoardsList) throw new Error(`Missing #${BoardsList.prefix} > header`);
+      const boardsList     = document.querySelector(`[data-prefix="${BoardsList.prefix}"] > ul`);
+      const deleteBoardBtn = component.querySelectorAll("button")[2];
+      if (!deleteBoardBtn) throw new Error(`deleteBoardBtn is missing`);
+      if (!boardsList)     throw new Error(`[data-prefix="${BoardsList.prefix}"] > <ul> is missing`);
 
-      const textContent = headerOfBoardsList.textContent;
-      const btnImage    = deleteBoardBtn.querySelector("img");
-      if (!textContent) throw new Error(`Missing content of #${BoardsList.prefix} > header`);
-      if (!btnImage)    throw new Error(`Missing deleteBoardBtn > img`);
+      const btnImage = deleteBoardBtn.querySelector("img");
+      if (!btnImage) throw new Error(`deleteBoardBtn <img> is missing`);
 
-      const countOfBoards = Number(textContent.slice(textContent.indexOf("(") + 1, textContent.indexOf(")")));
-      if (countOfBoards > 1) {
+      if (boardsList.children.length > 1) {
 	deleteBoardBtn.removeAttribute("disabled");
 	btnImage.src = "images/svg/icon-delete.svg";
       } else {
@@ -193,12 +211,11 @@ export class MainHeader {
     /**
      * Implements procedure of signout.
      *
-     * @throws {Error} Throws an error
-     * if authz_token could not be generating or response.status !== 204
+     * @throws {Error} Throws an error is response.status !== 204
      * @returns {Promise<void>}
      */
     async function signout() {
-      const url     = "http://localhost:3000/api/signout";
+      const url     = "/v1/signout";
       const options = {
 	method: "POST",
 	headers: {
@@ -207,44 +224,39 @@ export class MainHeader {
 	},
       };
 
-      // [Errors 400, 401, 403, 500] [Success 204]
+      // [Errors 401, 403, 404, 405, 500] [Success 204]
       let response = await fetch(url, options);
 
       if (response.status === 401) {
-	const resAuthz = await fetch("http://localhost:3000/api/generate_authz_token", { method: "POST" });
-	if (resAuthz.status === 401) {
-	  await openRedirectDialog();
+	// [Errors 400, 401, 500] [Success 201]
+	const responseBearer = await fetch("/v1/bearers/generate", { method: "POST" });
 
-	  return;
+	if (responseBearer.status !== 201) {
+	  if (responseBearer.status === 401) {
+	    const { openSessionExpiredDialog } = await import("../functions.js");
+	    await openSessionExpiredDialog();
+
+	    throw new Error("Session expired");
+	  } else {
+	    const { openPopUp } = await import("../../_shared/functions.js");
+	    await openPopUp("Authentication token error", "Something went wrong. Try again.");
+
+	    throw new Error("Authentication error");
+	  }
 	}
 
-	if (resAuthz.status !== 201) {
-	  const { PopUp } = await import("../../_shared/components/pop_up.js");
-	  document.body.appendChild(
-	    PopUp.init({
-	      title: "Authentication token error",
-	      message: "Something went wrong. Try again."
-	    })
-	  );
+	const bearer = await responseBearer.json();
 
-	  return;
-	}
-
-	localStorage.setItem("bearer", await resAuthz.json());
-	options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
+	localStorage.setItem("bearer", bearer);
+	options.headers.Authorization = `Bearer ${ bearer }`;
 	response = await fetch(url, options);
-      }
+      };
 
       if (response.status !== 204) {
-	const { PopUp } = await import("../../_shared/components/pop_up.js");
-	document.body.appendChild(
-	  PopUp.init({
-	    title: "Server error",
-	    message: "Something went wrong. Try again."
-	  })
-	);
+	const { openPopUp } = await import("../../_shared/functions.js");
+	openPopUp("Server error", "Something went wrong. Try again.");
 
-	return;
+	throw new Error("Server error");
       }
 
       localStorage.removeItem("bearer");
