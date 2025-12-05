43bc004f3af1aa346f452e93c10e6d2bbe9e6294
AlexKolykhalov <ak8647@rambler.ru>
2025-12-05 15:14:02 +0300

    guru:kanban-ssr-docker Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.
    3. Use CustomEvent directly without the custom "emit" function.

--- guru/kanban-task-management-ssr-docker/public/pages/index/components/boards_list_item.js
+++ guru/kanban-task-management-ssr-docker/public/pages/index/components/boards_list_item.js
@@ -1,10 +1,7 @@
 // @ts-check
 
-import { Board }      from "./board.js";
-import { MainHeader } from "./main_header.js";
-
 /**
- * @typedef {object} BoardsListItemType
+ * @typedef {object}   BoardsListItemType
  * @property {string}  id
  * @property {string}  title
  * @property {boolean} [selected] By default "false".
@@ -12,29 +9,23 @@ import { MainHeader } from "./main_header.js";
  */
 
 export class BoardsListItem {
-
-  static prefix   = "boards_list_item"; // using in edit_board_dialog.js
-  static selector = `[id^=${this.prefix}-]`;
+  static prefix = "boards-list-item"; // using in edit_board_dialog.js
 
   /**
    * @param {BoardsListItemType} props
-   *
    * @returns {string} HTML string
    */
   static template(props) {
-    const isSSR    = typeof window === "undefined" && typeof document === "undefined";
-    const attr     = props.locked   ? `data-locked="true"` : "";
-    const selected = props.selected ? "selected" : "";
-    const path     = isSSR          ? `data-path="http://localhost:3000/pages/index/components/boards_list_item.js"` : "";    
+    if (typeof window === "undefined" && typeof document === "undefined") // if SSR
+      globalThis.paths[this.prefix] = "/pages/index/components/boards_list_item.js";
 
-    return `<li id="${this.prefix}-${props.id}" class="boards-list-item ${selected}" ${attr} ${path}>
+    return `<li class="boards-list-item ${props.selected ? "selected" : ""}" data-prefix="${this.prefix}" data-id="${props.id}" ${props.locked ? "data-locked" : ""}>
               <img src="images/svg/${props.locked ? "icon-locked" : "icon-board-grey"}.svg" alt="" width="16" height="16">${props.title}
             </li>`;
   }
 
   /**
    * @param {BoardsListItemType} props
-   *
    * @returns {Element}
    */
   static init(props) {
@@ -43,14 +34,13 @@ export class BoardsListItem {
 
   /**
    * @param {BoardsListItemType} props
-   *
    * @returns {Element}
    */
   static #create(props) {
     const template     = document.createElement("template");
     template.innerHTML = this.template(props);
     const component    = template.content.firstElementChild;
-    if (!component)    throw new Error("Can't render \"BoardsListItem\" component");
+    if (!component)    throw new Error(`Can't create ${this.name} component`);
 
     this.handleEvents(component);
 
@@ -59,96 +49,111 @@ export class BoardsListItem {
 
   /**
    * @param {Element} component
-   *
    * @returns {void}
    */
   static handleEvents(component) {
-    // @ts-ignore
-    const isLocked = component.dataset.locked === "true"; delete component.dataset.locked;    
-
-    if (isLocked) component.addEventListener("click", async () => {
-      const { openAuthzDialog } = await import("./_helpers.js");
-      openAuthzDialog();
-    });
-    if (!isLocked) {
+    if (component.hasAttribute("data-locked")) {
       component.addEventListener("click", async function() {
-
 	if (this.classList.contains("selected")) return;
+
+	const { openAuthzDialog } = await import("../functions.js");
+	await openAuthzDialog();
+      });
+
+      component.removeAttribute("data-locked");
+    } else {
+      component.addEventListener("click", async function() {
+	if (this.classList.contains("selected"))   return;
+	if (document.querySelector(`.lds-ripple`)) return; // disable clicks on Tasks or BoardsList items
 
 	// add indicator
 	const { LoaderRipple } = await import("../../_shared/components/loader_ripple.js");
 	const loader = LoaderRipple.init();
 	component.appendChild(loader);
 
-	const id      = component.getAttribute("id")?.slice(`${BoardsListItem.prefix}-`.length);
-	const url     = `http://localhost:4000/rpc/get_board?p_board_id=${id}`;
+	const boardID = component.getAttribute("data-id");
+	if (!boardID) throw new Error("[data-id] is missing");
+
+	const url     = `/v1/boards/${boardID}`;
 	const options = {
-	  headers: {
-	    "Accept":        "application/vnd.pgrst.object+json",
-	    "Authorization": `Bearer ${ localStorage.getItem("bearer") }`,
-	  }
+	  headers: { "Authorization": `Bearer ${ localStorage.getItem("bearer") }` }
 	};
-	// [Errors 401, 403, 404] [Success 200]
+
+	// [Errors 401, 403, 404, 405, 500] [Success 200]
 	let response = await fetch(url, options);
 
 	if (response.status === 401) {
 	  // [Errors 400, 401, 500] [Success 201]
-	  const resAuthz = await fetch("http://localhost:3000/api/generate_authz_token", { method: "POST" });
-	  if (resAuthz.status === 401) {
-	    const { openRedirectDialog } = await import("./_helpers.js");
-	    openRedirectDialog();
+	  const responseBearer = await fetch("/v1/bearers/generate", { method: "POST" });
 
-	    return;
-	  }
+	  if (responseBearer.status !== 201) {
+	    if (responseBearer.status === 401) {
+	      const { openSessionExpiredDialog } = await import("../functions.js");
+	      await openSessionExpiredDialog();
+	    } else {
+	      const { openPopUp } = await import("../../_shared/functions.js");
+	      await openPopUp("Authentication token error", "Something went wrong. Try again.");
+	    }
 
-	  if (resAuthz.status !== 201) {
-	    const { PopUp } = await import("../../_shared/components/pop_up.js");
-	    document.body.appendChild(
-	      PopUp.init({ title: "Authentication token error", message: "Something went wrong. Try again." })
-	    );
 	    loader.remove();
+	    // @ts-ignore (mob-view click dropdownListToggleBtn)
+	    document.querySelector(".dropdown-list-toggle-btn")?.click();
 
 	    return;
 	  }
 
-	  localStorage.setItem("bearer", await resAuthz.json());
-	  options.headers.Authorization = `Bearer ${ localStorage.getItem("bearer") }`;
+	  const bearer = await responseBearer.json();
+
+	  localStorage.setItem("bearer", bearer);
+	  options.headers.Authorization = `Bearer ${ bearer }`;
 	  response = await fetch(url, options);
 	};
 
-	if (response.status !== 200) {
-	  const { PopUp } = await import("../../_shared/components/pop_up.js");
-	  document.body.appendChild(
-	    PopUp.init({ title: "Server error", message: "Something went wrong. Try again." })
-	  );
+	if (response.status === 403) {
+	  const { openAuthzDialog } = await import("../functions.js");
+	  await openAuthzDialog();
+
 	  loader.remove();
+	  // @ts-ignore (mob-view click dropdownListToggleBtn)
+	  document.querySelector(".dropdown-list-toggle-btn")?.click();
 
 	  return;
-	};
+	}
 
+	if (response.status === 404 || response.status !== 200) {
+	  const { openPopUp } = await import("../../_shared/functions.js");
+	  await openPopUp(
+	    response.status === 404 ? "Search error" : "Server error",
+	    response.status === 404 ? "Board not found" : "Something went wrong. Try again"
+	  );
+
+	  loader.remove();
+	  // @ts-ignore (mob-view click dropdownListToggleBtn)
+	  document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+	  return;
+	}
+
+	/** @type {import("./board.js").BoardType} */
 	const receivedBoardData = await response.json();
-
-	// update title of the boards_list_item
-	// Anonymous update board on client, not in DB, so refresh it in another click
-	component.childNodes[2].nodeValue = receivedBoardData.name;
-
-	const { emit } = await import("./_helpers.js");
-	emit(
-	  "board:selected",
-	  receivedBoardData,
-	  // @ts-ignore
-	  document.querySelector(`#${Board.prefix}`)
-	);	
-	emit(
-	  "board:selected",
-	  receivedBoardData,
-	  // @ts-ignore
-	  document.querySelector(`#${MainHeader.prefix}`)
-	);
+	component.childNodes[2].nodeValue = receivedBoardData.name; // update BoardsListItem title
+	const { Board }      = await import("./board.js");
+	const { MainHeader } = await import("./main_header.js");
+	const board      = document.querySelector(`[data-prefix="${Board.prefix}"]`);
+	const mainHeader = document.querySelector(`[data-prefix="${MainHeader.prefix}"]`);
+	if (!board)      throw new Error(`[data-prefix="${Board.prefix}"] is missing`);
+	if (!mainHeader) throw new Error(`[data-prefix="${MainHeader.prefix}"] is missing`);
 
 	this.parentElement.querySelector("li.selected")?.classList.remove("selected");
 	this.classList.add("selected");
+
 	loader.remove();
+	// @ts-ignore (mob-view click dropdownListToggleBtn)
+	document.querySelector(".dropdown-list-toggle-btn")?.click();
+
+	[board, mainHeader].forEach(item => {
+	  item.dispatchEvent(new CustomEvent("board:selected", { detail: receivedBoardData }));
+	});
       });
     }
   }
