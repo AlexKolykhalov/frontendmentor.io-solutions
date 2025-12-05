932dbb266271874b27956620ee9cebba499ff6c9
AlexKolykhalov <ak8647@rambler.ru>
2025-12-05 15:21:50 +0300

    guru:kanban-ssr-docker Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.

--- guru/kanban-task-management-ssr-docker/public/pages/index/components/control_btns.js
+++ guru/kanban-task-management-ssr-docker/public/pages/index/components/control_btns.js
@@ -3,14 +3,13 @@
 import { ColorSchemeToggle } from "../../_shared/components/color_scheme_toggle.js";
 
 export class ControlBtns {
-
-  static selector = `#control_btns`;
+  static prefix = "control-btns";
 
   /** @returns {string} HTML string */
   static template() {
-    const path  = `data-path="http://localhost:3000/pages/index/components/control_btns.js"`;
-    
-    return `<div id="control_btns" class="column gap-m pad-m" style="margin-bottom: 20px;" ${path}>
+    globalThis.paths[this.prefix] = "/pages/index/components/control_btns.js";
+
+    return `<div class="column gap-m pad-m" style="margin-bottom: 20px;" data-prefix="${this.prefix}">
               ${ColorSchemeToggle.template()}
               <button class="[ m:display-none ] hide-sidebar-btn hide" style="position: relative; left: -1rem; width: calc(100% + 1rem); padding-left: 2rem;">
                 <img src="images/svg/icon-hide-sidebar.svg" alt="">
@@ -21,17 +20,18 @@ export class ControlBtns {
 
   /**
    * @param {Element} component
-   *
    * @returns {void}
    */
   static handleEvents(component) {
     const hideSidebarBtn = component.querySelector(":scope > button");
-    if (!hideSidebarBtn) throw new Error("Missing :scope > button");
-    hideSidebarBtn.addEventListener("click", () => {
+    if (!hideSidebarBtn) throw new Error("<button> is missing");
+
+    hideSidebarBtn.addEventListener("click", async () => {
+      const { Board } = await import("./board.js");
       const sidebar = document.querySelector("main > .with-left-sidebar > :nth-child(1)");
-      const hideSidebarBtnInBoardComponent = document.querySelector("#board > button");
-      if (!sidebar) throw new Error("Missing main > .with-left-sidebar > :nth-child(1)");
-      if (!hideSidebarBtnInBoardComponent) throw new Error("Missing #board > button");
+      const hideSidebarBtnInBoardComponent = document.querySelector(`[data-prefix="${Board.prefix}"] > button`);
+      if (!sidebar) throw new Error(".with-left-sidebar > :nth-child(1) is missing");
+      if (!hideSidebarBtnInBoardComponent) throw new Error(`[data-prefix="${Board.prefix}"] > <button> is missing`);
 
       sidebar.classList.add("md:display-none");
       hideSidebarBtnInBoardComponent.classList.remove("md:display-none");
