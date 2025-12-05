149f9c68f5035070d14e868e16d181b25faeaa99
AlexKolykhalov <ak8647@rambler.ru>
2025-12-05 15:32:23 +0300

    guru:kanban-ssr-docker Optimized the component
    1. Choose "data-*" attribute to store data instead of "id" selector.
    2. Changed errors text.
    3. Use CustomEvent directly without the custom "emit" function.

--- guru/kanban-task-management-ssr-docker/public/pages/index/components/dynamic_list_item.js
+++ guru/kanban-task-management-ssr-docker/public/pages/index/components/dynamic_list_item.js
@@ -1,8 +1,6 @@
 // @ts-check
 
-import { EditBoardDialog } from "./edit_board_dialog.js";
-import { EditTaskDialog } from "./edit_task_dialog.js";
-import { generateRandomSymbols, emit } from "./_helpers.js"
+import { generateRandomSymbols } from "../functions.js";
 
 /**
  * @typedef {object} DynamicListItemType
@@ -14,26 +12,24 @@ import { generateRandomSymbols, emit } from "./_helpers.js"
 
 export class DynamicListItem {
   /**
-   * @param {DynamicListItemType} [props]
-   *
+   * @param {DynamicListItemType} props
    * @returns {string} HTML string
    */
   static #template(props) {
-    const id          = props ? props.id ?? generateRandomSymbols(4) : generateRandomSymbols(4);
-    const value       = props ? props.value ?? "" : "";
-    const placeholder = props ? props.placeholder ?? "dynamic listitem" : "dynamic listitem";
-    const disabled    = props ? props.deleteBtnDisabled ?? false : false;
+    const id          = generateRandomSymbols(4);
+    const value       = props.value ?? "";
+    const placeholder = props.placeholder ?? "dynamic listitem";
+    const disabled    = props.deleteBtnDisabled ?? false;
 
-    return `<li id="dli-${id}" class="row cross-axis-center no-wrap">
-              <label class="sr-only" for="x-${id}">Dynamic list item ${id}</label>
-              <input class="[ flex-1 ] pad-sm fs-300 fw-medium clr-n-900-000 bg-n-100-900" id="x-${id}" value="${value}" placeholder="${placeholder}"">
+    return `<li class="row cross-axis-center no-wrap">
+              <label class="sr-only" for="${id}">Dynamic list item ${id}</label>
+              <input class="[ flex-1 ] pad-sm fs-300 fw-medium clr-n-900-000 bg-n-100-900" id="${id}" value="${value}" placeholder="${placeholder}" ${props.id ? `data-id="${props.id}"` : ""}>
               <button class="close-btn" aria-label="remove" ${disabled ? "disabled" : ""}></button>
             </li>`;
   }
 
   /**
-   * @param {DynamicListItemType} [props]
-   *
+   * @param {DynamicListItemType} props
    * @returns {Element}
    */
   static init(props) {
@@ -41,15 +37,14 @@ export class DynamicListItem {
   }
 
   /**
-   * @param {DynamicListItemType} [props]
-   *
+   * @param {DynamicListItemType} props
    * @returns {Element}
    */
   static #create(props) {
     const template     = document.createElement("template");
     template.innerHTML = this.#template(props);
     const component    = template.content.firstElementChild;
-    if (!component)    throw new Error("Can't create \"DynamicListItem\" component");
+    if (!component)    throw new Error(`Can't create ${this.name} component`);
 
     this.#handleEvents(component);
 
@@ -63,33 +58,26 @@ export class DynamicListItem {
   static #handleEvents(component) {
     const input     = component.querySelector("input");
     const removeBtn = component.querySelector("button");
-
-    if (!input)     throw new Error("Can't find <input>");
-    if (!removeBtn) throw new Error("Can't find <button>");
+    if (!input)     throw new Error("<input> is missing");
+    if (!removeBtn) throw new Error("<button> is missing");
 
     input.addEventListener("input", function() {
-      const editBoardDialog = document.querySelector(`#${EditBoardDialog.prefix}`);
-      const editTaskDialog  = document.querySelector(`#${EditTaskDialog.prefix}`);
-
-      if (editBoardDialog) emit("dynamic-list-item:changed", {}, editBoardDialog);
-      if (editTaskDialog)  emit("dynamic-list-item:changed", {}, editTaskDialog);
-
       this.removeAttribute("style");
+      // listens to EditBoardDialog or EditTaskDialog
+      component.dispatchEvent(new CustomEvent("dynamic-list-item:changed", { bubbles: true }));
+
+      console.log("dynamic-list-item:changed");
     });
 
     removeBtn.addEventListener("click", function() {
-      const listOfItems = removeBtn.closest("ul");
-      if (!listOfItems) throw new Error("Can't find <ul>");
-
-      const dynamicList = listOfItems.parentElement;
-      if (!dynamicList) throw new Error("Can't find parent element for <ul>");
+      const parent = component.parentElement;
+      if (!parent) throw new Error("Parent component is missing");
 
       component.remove();
-      emit("dynamic-list-item:removed", {}, dynamicList);
-      const editBoardDialog = document.querySelector(`#${EditBoardDialog.prefix}`);
-      const editTaskDialog  = document.querySelector(`#${EditTaskDialog.prefix}`);
-      if (editBoardDialog) emit("dynamic-list-item:removed", {}, editBoardDialog);
-      if (editTaskDialog)  emit("dynamic-list-item:removed", {}, editTaskDialog);
+      // listens to DynamicList and EditBoardDialog or EditTaskDialog
+      parent.dispatchEvent(new CustomEvent("dynamic-list-item:removed", { bubbles: true }));
+
+      console.log("dynamic-list-item:removed");
     });
   }
 }
